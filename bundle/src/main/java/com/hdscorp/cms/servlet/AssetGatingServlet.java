8ed2ebe4a9ef5be8125186f001ad268d6d5d1e19
china.yadlapalli@hds.com <china.yadlapalli@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2015-12-15 22:56:27 +0000

    Committing the changes required for Marketo service to trigger.
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@311 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/servlet/AssetGatingServlet.java
+++ bundle/src/main/java/com/hdscorp/cms/servlet/AssetGatingServlet.java
@@ -1,50 +1,45 @@
 package com.hdscorp.cms.servlet;
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.servlets.SlingAllMethodsServlet;
- 
-import javax.servlet.ServletException;
-import java.io.IOException;
+import org.apache.sling.api.request.RequestDispatcherOptions;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
  
 @SlingServlet(
-    //resourceTypes = {"dam:Asset"},
-    methods = {"GET"},
-    extensions = {"pdf"}
+		resourceTypes = {"dam:Asset"},
+	    methods = {"GET"},
+	    extensions = {"pdf"}
 )
 @Properties({
     @Property(name="service.pid", value="com.hdscorp.cms.servlet.AssetGatingServlet",propertyPrivate=false),
     @Property(name="service.description",value="Asset Gating Servlet", propertyPrivate=false),
     @Property(name="service.vendor",value="HDS Corp", propertyPrivate=false)
 })
-public class AssetGatingServlet extends SlingAllMethodsServlet
+public class AssetGatingServlet extends SlingSafeMethodsServlet
 {
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException
     {
+    	
+    	final RequestDispatcherOptions options = new RequestDispatcherOptions();
+    	options.setForceResourceType("dam/asset");
         //Do something fun here
     	String forwardPath= "";
     	System.out.println("*********************************************************");
-    	System.out.println("**************************dindidnidnididnid*******************************");
+    	System.out.println("**************************dindidnidnididnid in get method*******************************");
     	System.out.println("*********************************************************");
     	System.out.println("*********************************************************");
-    	
+    	request.getRequestDispatcher(request.getResource(), options).forward(request, response);
     }
  
-    @Override
-    protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException
-    {
-        //Do something fun here
-    	String forwardPath= "";
-    	System.out.println("*********************************************************");
-    	System.out.println("**************************dindidnidnididnid*******************************");
-    	System.out.println("*********************************************************");
-    	System.out.println("*********************************************************");
-    	
-    }
+   
     
 }
