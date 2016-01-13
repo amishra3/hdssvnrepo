88da01bca2183c916d792037aedb9c95bcb3c08e
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-01-13 11:33:20 +0000

    added code for twitter, facebook sling model
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@342 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ bundle/src/main/java/com/hdscorp/cms/slingmodels/FacebookFeedModel.java
@@ -0,0 +1,63 @@
+/*
+ * Decompiled with CFR 0_110.
+ * 
+ * Could not load the following classes:
+ *  com.hdscorp.cms.util.MultifieldUtil
+ *  javax.inject.Inject
+ *  javax.inject.Named
+ *  org.apache.sling.api.resource.Resource
+ *  org.apache.sling.models.annotations.Default
+ *  org.apache.sling.models.annotations.Model
+ */
+package com.hdscorp.cms.slingmodels;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = { Resource.class })
+public class FacebookFeedModel {
+	@Inject
+	@Named("jcr:fetitle")
+	@Default(values = { "title" })
+	private String title;
+	@Inject
+	@Named(value = "jcr:febackgroundimage")
+	@Default(values = { "/content/dam/geometrixx-outdoors/logo.png" })
+	private String bGImagePath;
+	@Inject
+	@Named(value = "jcr:feimportpath")
+	@Default(values = { "Facebook Import Path" })
+	private String importPath;
+	@Inject
+	@Named(value = "jcr:fefacbookiconpath")
+	@Default(values = { "/content/dam/geometrixx-outdoors/logo.png" })
+	private String iconPath;
+	@Inject
+	@Named(value = "jcr:fesocialfollowuslabel")
+	@Default(values = { "Social FollowUS Label" })
+	private String socialFollowUsLabel;
+
+	public String getSocialFollowUsLabel() {
+		return this.socialFollowUsLabel;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getbGImagePath() {
+		return this.bGImagePath;
+	}
+
+	public String getImportPath() {
+		return this.importPath;
+	}
+
+	public String getIconPath() {
+		return this.iconPath;
+	}
+}
