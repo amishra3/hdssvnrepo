3bbfad7460bada78772c6db34c7c6551fa66ea72
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-01-13 11:31:26 +0000

    added code for twitter, facebook and brighttalk scheduler
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@341 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/scheduler/BrightTalkScheduler.java
+++ bundle/src/main/java/com/hdscorp/cms/scheduler/BrightTalkScheduler.java
@@ -4,30 +4,33 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.jcr.Node;
-import javax.jcr.Session;
-
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.commons.scheduler.Scheduler;
 import org.apache.sling.jcr.api.SlingRepository;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hdscorp.cms.constants.ServiceConstants;
 import com.hdscorp.cms.restservice.BrightTalkWebService;
-import com.hdscorp.cms.restservice.FeedConstant;
-import com.hdscorp.cms.util.JcrUtilService;
+import com.hdscorp.cms.util.ServiceUtil;
 
+/**
+ * This is a BrightTalk Scheduler service is used for getting feed posts from Brighttalk feed.
+ * @author gokula.nand
+ */
 @Component(label = "BrightTalk Scheduler", description = "This service basically is used for consuming data from the feed", metatype = true, immediate = true)
 @Service(BrightTalkScheduler.class)
 @Properties({
-		@Property(name = "feed.url", description = "Default feed URL is provided you can change it accordingly", value = "https://www.brighttalk.com/channel/12821/feed"),
-		@Property(name = "scheduler.expression", description = "Default Cron Job", value = "0 25 13 * * ?"),
-		@Property(name = "storage.path", description = "Default storage path", value = "/content/hdscorp/en_us/test/jcr:content/") })
+		@Property(name = ServiceConstants.FEED_URL_KEY, description = "Default feed URL is provided you can change it accordingly", value = "https://www.brighttalk.com/channel/12821/feed"),
+		@Property(name = ServiceConstants.FEED_SCHEDULER_EXPRESSION, description = "Default Cron Job", value = "0 25 13 * * ?"),
+		@Property(name = ServiceConstants.FEED_STORAGE_PATH, description = "Default storage path", value = "/content/hdscorp/en_us/test/jcr:content/") })
 
 public class BrightTalkScheduler {
 	private static final Logger log = LoggerFactory.getLogger(BrightTalkScheduler.class);
@@ -40,22 +43,33 @@ public class BrightTalkScheduler {
 
 	@Reference
 	private BrightTalkWebService brightTalkService;
-	
+
+	@Reference
+	private ResourceResolverFactory resolverFactory;
+
 	private String schedulerExpression;
 
 	private String feedURL;
 
 	private String storagePath;
 
+	/**
+	 * Useful to running scheduler based on OSGI config properties.
+	 * @param ctx
+	 */
+	@Activate
 	protected void activate(ComponentContext ctx) {
-		this.schedulerExpression = ctx.getProperties().get("scheduler.expression").toString();
-		this.feedURL = ctx.getProperties().get("feed.url").toString();
-		this.storagePath = ctx.getProperties().get("storage.path").toString();
+		log.info("Execution start for activate method of BrightTalk Schedular");
+		this.schedulerExpression = ctx.getProperties().get(ServiceConstants.FEED_SCHEDULER_EXPRESSION).toString();
+		this.feedURL = ctx.getProperties().get(ServiceConstants.FEED_URL_KEY).toString();
+		this.storagePath = ctx.getProperties().get(ServiceConstants.FEED_STORAGE_PATH).toString();
 		Map<String, Serializable> configOne = new HashMap<>();
 
 		final Runnable job = new Runnable() {
-			public void run() {			
-				saveWSResponse(brightTalkService.getBrightTalkResponse(feedURL));				
+			public void run() {
+				log.info("started brightTalk scheduler");
+				ServiceUtil.saveWSResponse(brightTalkService.getBrightTalkResponse(feedURL), storagePath,
+						ServiceConstants.SAVE_FEED_DATA_PROPERTY_NAME);
 			}
 		};
 		try {
@@ -65,25 +79,7 @@ public class BrightTalkScheduler {
 			job.run();
 		}
 
-	}
-
-	private void saveWSResponse(String wsResponse) {
-		Session session = null;
-		try {
-			session =JcrUtilService.getSession();
-			Node node = session.getNode(storagePath);
-			node.setProperty(FeedConstant.SAVE_FEED_DATA_PROPERTY_NAME, wsResponse);
-			session.save();
-
-		} catch (Exception e) {
-			log.error("Exception occurs duing saving data to JCR: ", e);
-		} finally {
-			if (session != null && session.isLive()) {
-				session.logout();
-				session = null;
-			}
-
-		}
+		log.info("Execution end for activate method of BrightTalk Schedular");
 	}
 
 }
