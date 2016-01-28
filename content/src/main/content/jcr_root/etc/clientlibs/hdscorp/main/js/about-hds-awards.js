a77aa3d3c08c59a0a05b1c77cf67b30fb611360f
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-01-28 02:12:33 +0000

    FED updates ingested
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@463 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/about-hds-awards.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/about-hds-awards.js
@@ -6,23 +6,27 @@ var hds = window.hds || {};
         init: function() {
             hds.abouthdsAwards.loadCatagoryHTML();
             hds.abouthdsAwards.bindEventsOnResize();
-            hds.abouthdsAwards.bindClick();
+            hds.abouthdsAwards.bindClick(); 
+            hds.abouthdsAwards.searchAwards();           
         },
+        
         loadCatagoryHTML: function() {
             hds.abouthdsAwards.processHTML($('.linkLeft a').eq(0).attr('data-loadhtml'), 0);
             $('.linkLeft a').eq(0).addClass('active');
             $('.linkLeft a').eq(0).parent().addClass('active');
+            
         },
         processHTML: function(url, index) {
-            $("#contentCatagory").html(" ").load(url, function() {
+            $("#awardsContent").html(" ").load(url, function() {
                 hds.abouthdsAwards.bindHTMLLoad();
+                hds.abouthdsAwards.loadMoreAwards();
             });
         },
         bindHTMLLoad: function() {
             if ($(window).width() < 991) {
                 $('.linkLeft a').each(function() {
                     if ($(this).hasClass('active')) {
-                        $(this).parent('li').find('.MobileHolderWrapper').append($('#contentCatagory').html());
+                        $(this).parent('li').find('.MobileHolderWrapper').append($('#awardsContent').html());
                         $('#contentCatagory').empty();
                         $(this).parent('li').find('.icon-accordion-closed').css('display', 'none');
                         $(this).parent('li').find('.icon-accordion-opened').css('display', 'inline-block');
@@ -42,6 +46,48 @@ var hds = window.hds || {};
                 });
             }
         },
+        loadMoreAwards:function () {
+         var sizeAwardsList = $(".pr").hide().size(),
+            x = 4;
+            $('#loadMoreAwardsBtn').removeAttr('style');
+            $('.pr:lt(' + x + ')').show();
+            if(sizeAwardsList <= 4){
+                $('#loadMoreAwardsBtn').hide();
+            }
+            $(document).on('click','#loadMoreAwardsBtn', function() {
+                x = (x + 5 <= sizeAwardsList) ? x + 5 : sizeAwardsList;
+                $('.pr:lt(' + x + ')').show();                
+                if (x == sizeAwardsList) {
+                    $('#loadMoreAwardsBtn').hide();
+                }
+            });
+        },
+        searchAwards:function(){
+              jQuery.expr[':'].Contains = function(a, i, m) {
+                return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+            };
+
+            $(document).on('keyup change', '#awardsSearch', function(event) {
+                var getSearchFilter = $(this).val();
+                $('#awardsContent').find('.no-matched-result').remove();
+                if (getSearchFilter.length > 0) {  
+                console.log($(this).val());
+                 $('#loadMoreAwardsBtn').hide();
+                 $(".pr").show();
+                 $(".pr").find("h3.filterText:not(:Contains(" + getSearchFilter + "))").parents('div.pr').hide();
+                 $(".pr").find("h3.filterText:Contains(" + getSearchFilter + ")").parents('div.pr').show();
+           
+            
+                }else{
+                    $(".pr").show();
+                } 
+                if($(".pr:visible").length === 0){
+                
+                $('#awardsContent').append('<div class="no-matched-result" style="padding: 50px 0; text-align: center;">No results found.</div>');
+                }
+                event.preventDefault();
+            });
+        },
         bindEventsOnResize: function() {
             $(window).resize(function() {
                 hds.abouthdsAwards.bindHTMLLoad();
@@ -51,7 +97,7 @@ var hds = window.hds || {};
             $('.linkLeft a').on('click', function(event) {
                 event.preventDefault();
                 var self = $(this);
-                
+                $('#awardsSearch').val(' ');
                     if (!$(this).hasClass('active')) {
                         $('.linkLeft a').removeClass('active');
                         $('.linkLeft a').parent().removeClass('active');
@@ -59,10 +105,8 @@ var hds = window.hds || {};
                             loadIndec = $(this).parent().index();
                         $(this).addClass('active');
                         $(this).parent().addClass('active');
-
                         $('.linkLeft').find('.icon-accordion-closed').removeAttr('style').css('display', 'inline-block');
                         $('.linkLeft').find('.icon-accordion-opened').removeAttr('style').css('display', 'none');
-
                         $('.linkLeft a').parent().find('.MobileHolderWrapper').html(" ").removeAttr("style");
                         hds.abouthdsAwards.processHTML(loadUrl, loadIndec);
                     } else if ($(this).hasClass('active') && $(window).width() < 991) {
@@ -80,8 +124,7 @@ var hds = window.hds || {};
         }
     }
 }(window, document, jQuery, hds));
-
-$(function() {	
+$(function() {
 	if ( $('.about-hds-awards').length > 0){
 		hds.abouthdsAwards.init();
 	}
