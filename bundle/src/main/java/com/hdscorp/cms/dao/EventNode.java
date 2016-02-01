d859c1406f3bee7790e766676a576895d3bab0e2
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-01 09:34:56 +0000

    added comparator in EventNode.java File and Adde TagResults for tag id and tagName storing
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@472 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/dao/EventNode.java
+++ bundle/src/main/java/com/hdscorp/cms/dao/EventNode.java
@@ -1,5 +1,7 @@
 package com.hdscorp.cms.dao;
 
+import java.util.Comparator;
+
 /**
  * Useful for Event Data Model
  * 
@@ -24,10 +26,22 @@ public class EventNode {
 
 	private String eventRegisterNowLabel;
 
-	private String eventRegisterNowLink;
+	private String eventRegisterNowLink;	
 
 	private String[] eventRegion;
+	
+	private String eventMonth;
 
+	private String eventTyptagId;
+	
+	private String eventTyptagName;
+	
+	private String eventRegiontagId;
+	
+	private String eventRegiontagName;
+	
+	private int year;
+	
 	public String[] getEventType() {
 		return eventType;
 	}
@@ -106,6 +120,83 @@ public class EventNode {
 
 	public void setEventRegion(String[] eventRegion) {
 		this.eventRegion = eventRegion;
+	}
+
+	public String getEventMonth() {
+		return eventMonth;
+	}
+
+	public void setEventMonth(String eventMonth) {
+		this.eventMonth = eventMonth;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getEventTyptagId() {
+		return eventTyptagId;
+	}
+
+	public void setEventTyptagId(String eventTyptagId) {
+		this.eventTyptagId = eventTyptagId;
+	}
+
+	public String getEventTyptagName() {
+		return eventTyptagName;
+	}
+
+	public void setEventTyptagName(String eventTyptagName) {
+		this.eventTyptagName = eventTyptagName;
+	}
+
+	public String getEventRegiontagId() {
+		return eventRegiontagId;
+	}
+
+	public void setEventRegiontagId(String eventRegiontagId) {
+		this.eventRegiontagId = eventRegiontagId;
+	}
+
+	public String getEventRegiontagName() {
+		return eventRegiontagName;
+	}
+
+	public void setEventRegiontagName(String eventRegiontagName) {
+		this.eventRegiontagName = eventRegiontagName;
+	}
+	
+	
+	public class CompareByMonth implements Comparator<EventNode> {
+		public int compare(EventNode p1, EventNode p2) {
+			if (p1.getEventMonth().compareTo(p2.getEventMonth()) > 0) {
+				return 1;
+			}
+			if (p1.getEventMonth().compareTo(p2.getEventMonth()) < 0) {
+				return -1;
+			}			
+			return 0;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return super.equals(obj);
+		}
+	}
+
+	public class CompareByYear implements Comparator<EventNode> {
+		public int compare(EventNode p1, EventNode p2) {
+			return p1.getYear() - p2.getYear();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return super.equals(obj);
+		}
 	}
 
 }
---
+++ bundle/src/main/java/com/hdscorp/cms/dao/TagResults.java
@@ -0,0 +1,55 @@
+package com.hdscorp.cms.dao;
+
+/**
+ * Useful for Tag Id and Tag Name in the components.
+ * 
+ * @author gokula.nand
+ *
+ */
+public class TagResults {
+	 private String tagId;
+	 private String tagName;
+	 
+	public String getTagId() {
+		return tagId;
+	}
+	public void setTagId(String tagId) {
+		this.tagId = tagId;
+	}
+	public String getTagName() {
+		return tagName;
+	}
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((tagId == null) ? 0 : tagId.hashCode());
+		result = prime * result + ((tagName == null) ? 0 : tagName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TagResults other = (TagResults) obj;
+		if (tagId == null) {
+			if (other.tagId != null)
+				return false;
+		} else if (!tagId.equals(other.tagId))
+			return false;
+		if (tagName == null) {
+			if (other.tagName != null)
+				return false;
+		} else if (!tagName.equals(other.tagName))
+			return false;
+		return true;
+	}
+	 
+}
