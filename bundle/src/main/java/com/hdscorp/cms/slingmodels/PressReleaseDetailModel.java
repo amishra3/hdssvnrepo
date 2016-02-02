912488c28031c7d6c529005a29614ca9d619ee5a
china.yadlapalli@hds.com <china.yadlapalli@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-02 21:50:02 +0000

    Committing press releases detail model changes for view all link
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@503 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/slingmodels/PressReleaseDetailModel.java
+++ bundle/src/main/java/com/hdscorp/cms/slingmodels/PressReleaseDetailModel.java
@@ -4,12 +4,16 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Default;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
+
+import com.day.cq.wcm.api.Page;
+import com.hdscorp.cms.util.PathResolver;
 
 @Model(adaptables = Resource.class)
 public class PressReleaseDetailModel {
-
+	@Inject
+    private Page resourcePage;
 	@Inject
 	@Named("pressreleasetitle")
 	private String pressReleaseTitle;
@@ -21,6 +25,19 @@ public class PressReleaseDetailModel {
 	@Inject
 	@Named("pressreleasedesc")
 	private String pressReleaseDescription;
+	
+	private String viewAllPRLabel;
+	private String viewAllPRLink;
+	public String getViewAllPRLabel() {
+
+	return getProperty("viewallprlabel");
+	}
+
+	public String getViewAllPRLink() {
+		return PathResolver.getShortURLPath(getProperty("viewallprlink"));
+	}
+
+	
 
 	public String getPressReleaseTitle() {
 		return pressReleaseTitle;
@@ -34,6 +51,20 @@ public class PressReleaseDetailModel {
 		return pressReleaseDescription;
 	}
 
+	
+	private String getProperty(String property){
+		String value = "";
+		Page page = resourcePage.getAbsoluteParent(4);
+		if(page!=null) {
+			
+		ValueMap properties =  page.getProperties();
+		if(properties.containsKey(property)){
+		value = properties.get(property).toString();
+		}
+		}
+		
+		return value;
+	}
 	
 
 }
