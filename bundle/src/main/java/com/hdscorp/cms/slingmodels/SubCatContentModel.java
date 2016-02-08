3ae254763331e7c2b74e6a61bde9682fb10283b0
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-08 05:46:33 +0000

    sub cat model service updated
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@576 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/slingmodels/SubCatContentModel.java
+++ bundle/src/main/java/com/hdscorp/cms/slingmodels/SubCatContentModel.java
@@ -1,5 +1,6 @@
 package com.hdscorp.cms.slingmodels;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,11 @@ import javax.jcr.RepositoryException;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +21,7 @@ import com.day.cq.search.result.Hit;
 import com.day.cq.search.result.SearchResult;
 import com.day.cq.wcm.api.Page;
 import com.hdscorp.cms.config.HdsCorpGlobalConfiguration;
+import com.hdscorp.cms.dao.ProductDescription;
 import com.hdscorp.cms.dao.ProductNode;
 import com.hdscorp.cms.search.SearchServiceHelper;
 import com.hdscorp.cms.util.PathResolver;
@@ -42,37 +48,58 @@ public class SubCatContentModel {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SubCatContentModel.class);
 	
-	public List<ProductNode> getProducts() throws RepositoryException {
+	public List<ProductNode> getProducts() throws RepositoryException, JsonParseException, JsonMappingException, IOException {
 		
-		LOG.debug("-------------INSIDE getProducts in SubCatContentModel.Making the Search Service call");
+		try {
+			LOG.debug("-------------INSIDE getProducts in SubCatContentModel.Making the Search Service call");
 
-		SearchServiceHelper searchServiceHelper = (SearchServiceHelper)ViewHelperUtil.getService(com.hdscorp.cms.search.SearchServiceHelper.class);
-        
-		String paths[] = {"/content/hdscorp/en_us/products-solutions"};
-		String tags[] = subcattags ;
-		String template= "/apps/hdscorp/templates/productdetail";
-		String type[] = {"cq:Page"};
-		boolean doPagination = false;
-		
-		SearchResult result = searchServiceHelper.getFullTextBasedResuts(paths,tags,template,type,null,doPagination,null,null,resourceResolver,null,null);
-		
-		LOG.debug("-------------SEARCH CALL COMPLETED-----"+result.getTotalMatches());
-		List<Hit> hits = result.getHits();
-		products = new ArrayList<ProductNode>();
-		
-		for (Hit hit : hits) {
-			ProductNode productNode = new ProductNode();
-			Page reourcePage = hit.getResource().adaptTo(Page.class);
-		    String pageTitle = reourcePage.getTitle();
-		    String pagePath = reourcePage.getPath();
-		    String pageProductDescription = (String)reourcePage.getProperties().get("subtext");
-		    if(pagePath.startsWith("/content")){
-		    	pagePath=PathResolver.getShortURLPath(pagePath);
-		    }
-		    productNode.setProductTitle(pageTitle);
-		    productNode.setProductDescription(pageProductDescription);
-		    productNode.setProductPath(pagePath);
-		    products.add(productNode);
+			SearchServiceHelper searchServiceHelper = (SearchServiceHelper)ViewHelperUtil.getService(com.hdscorp.cms.search.SearchServiceHelper.class);
+			
+			String paths[] = {"/content/hdscorp/en_us/products-solutions"};
+			String tags[] = subcattags ;
+			String template= "/apps/hdscorp/templates/productdetail";
+			String type[] = {"cq:Page"};
+			boolean doPagination = false;
+			
+			SearchResult result = searchServiceHelper.getFullTextBasedResuts(paths,tags,template,type,null,doPagination,null,null,resourceResolver,null,null);
+			
+			LOG.debug("-------------SEARCH CALL COMPLETED-----"+result.getTotalMatches());
+			List<Hit> hits = result.getHits();
+			products = new ArrayList<ProductNode>();
+			
+			for (Hit hit : hits) {
+				ProductNode productNode = new ProductNode();
+				Page reourcePage = hit.getResource().adaptTo(Page.class);
+			    String pageTitle = reourcePage.getTitle();
+			    String pagePath = reourcePage.getPath();
+			    String pageProductDescription = (String)reourcePage.getProperties().get("subtext");
+			    
+			    Resource descriptionListResource = reourcePage.getContentResource("productdescriptions") ;
+			    String[] productMultiDescriptionList = new String[0];
+			    ObjectMapper mapper = new ObjectMapper();
+				ArrayList<ProductDescription> descriptionList = new ArrayList<ProductDescription>();
+			    if(descriptionListResource!=null){
+			    	ValueMap descriptioNodeProps= descriptionListResource.adaptTo(ValueMap.class);
+			    	productMultiDescriptionList = descriptioNodeProps.get("descriptionlist",new String[0]);
+				    for(String desc:productMultiDescriptionList){
+				    	ProductDescription prodDescObj = mapper.readValue(desc, ProductDescription.class);
+				    	descriptionList.add(prodDescObj);
+				    }
+			    }
+
+			    
+			    if(pagePath.startsWith("/content")){
+			    	pagePath=PathResolver.getShortURLPath(pagePath);
+			    }
+			    productNode.setProductTitle(pageTitle);
+			    productNode.setProductDescription(pageProductDescription);
+			    productNode.setProductPath(pagePath);
+			    products.add(productNode);
+			    productNode.setDescriptionList(descriptionList);
+			}
+		} catch (Exception e) {
+			LOG.error("----IN EXCEPTION BLOCK----"+e.getCause());
+			LOG.error(e.getMessage());
 		}
 		return products;
 	}
