88a65cd2c60971771d49269f609524f1e2676eab
sandeep.kaur@hds.com <sandeep.kaur@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-23 00:58:02 +0000

    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@862 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/workflow/AddTitleWorkflowProcess.java
+++ bundle/src/main/java/com/hdscorp/cms/workflow/AddTitleWorkflowProcess.java
@@ -26,66 +26,55 @@ import org.slf4j.LoggerFactory;
 @org.apache.felix.scr.annotations.Property(name="process.label", value={"Overwrite PDF Title"})
 public class AddTitleWorkflowProcess extends AbstractAssetWorkflowProcess
 {
-  private static final Logger log = LoggerFactory.getLogger(AddTitleWorkflowProcess.class);
-  private static final String JCR_CONTENT_METADATA = "jcr:content/metadata";
-  private static final int BATCH_SIZE = 500;
+	private static final Logger log = LoggerFactory.getLogger(AddTitleWorkflowProcess.class);
 
-  public boolean overwriteTitle(Asset asset)
-    throws RepositoryException
-  {
-	  log.info("********************AddTitleWorkflowProcess**overwriteTitle***********");
-    Rendition original = asset.getOriginal();
-    Resource res = (Resource)asset.adaptTo(Resource.class);
-    Resource metadata = res.getChild("jcr:content/metadata");
-    System.out.println("******************metadata*********************"+metadata);
+	public boolean overwriteTitle(Asset asset)
+			throws RepositoryException
+			{
+		Rendition original = asset.getOriginal();
+		Resource res = (Resource)asset.adaptTo(Resource.class);
+		Resource metadata = res.getChild("jcr:content/metadata");
+		Node metadataNode;
+		if (metadata != null)
+			metadataNode = (Node)metadata.adaptTo(Node.class);
+		else
+			return false;
+		//Node metadataNode;
+		metadataNode.setProperty("dc:title",metadataNode.getProperty("hds:title").getValue());
+		Node originalBinary = (Node)original.adaptTo(Node.class);
 
-	log.info("********************AddTitleWorkflowProcess**metatdata***********"+metadata);
-    Node metadataNode;
-    if (metadata != null)
-      metadataNode = (Node)metadata.adaptTo(Node.class);
-    else
-      return false;
-    //Node metadataNode;
-    System.out.println("HDS TItLE*******************"+metadataNode.getProperty("hds:title").getValue());
-    log.info("********************AddTitleWorkflowProcess***hds title**********"+metadataNode.getProperty("hds:title").getValue());
-    metadataNode.setProperty("dc:title",metadataNode.getProperty("hds:title").getValue());
-    Node originalBinary = (Node)original.adaptTo(Node.class);
+		return true;
 
-     return true;
-    
-  }
-  private Resource getPayloadResource(WorkItem item, Session session) {
-    Resource resource = null;
-    if (item.getWorkflowData().getPayloadType().equals("JCR_PATH")) {
-      String path = item.getWorkflowData().getPayload().toString();
-      resource = getResourceResolver(session).getResource(path);
-    }
-    return resource;
-  }
+			}
+	private Resource getPayloadResource(WorkItem item, Session session) {
+		Resource resource = null;
+		if (item.getWorkflowData().getPayloadType().equals("JCR_PATH")) {
+			String path = item.getWorkflowData().getPayload().toString();
+			resource = getResourceResolver(session).getResource(path);
+		}
+		return resource;
+	}
 
-  public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaData) throws WorkflowException
-  {
-    try
-    {
-    	log.info("********************AddTitleWorkflowProcess*************");
-      Session session = workflowSession.getSession();
-      Resource resource = getPayloadResource(workItem, session);
-      log.info("********************AddTitleWorkflowProcess***resource**********"+resource.getResourceType());
-      if (null != resource) {
-    	  log.info("********************AddTitleWorkflowProcess***DamUtil.isAsset(resource)**********"+DamUtil.isAsset(resource));
-         Asset asset = DamUtil.resolveToAsset(resource);
-          overwriteTitle(asset);
-          return;
-        
-      }
-      else
-      {
-        log.error("Content root could not be resolved");
-      }
-    }
-    catch (Exception e)
-    {
-      log.warn("unexpected error occurred during adding Property. Cause: {}", e.getMessage(), e);
-    }
-  }
+	public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaData) throws WorkflowException
+	{
+		try
+		{
+			Session session = workflowSession.getSession();
+			Resource resource = getPayloadResource(workItem, session);
+			if (null != resource) {
+				Asset asset = DamUtil.resolveToAsset(resource);
+				overwriteTitle(asset);
+				return;
+
+			}
+			else
+			{
+				log.error("Content root could not be resolved");
+			}
+		}
+		catch (Exception e)
+		{
+			log.warn("unexpected error occurred during adding Property. Cause: {}", e.getMessage(), e);
+		}
+	}
 }
