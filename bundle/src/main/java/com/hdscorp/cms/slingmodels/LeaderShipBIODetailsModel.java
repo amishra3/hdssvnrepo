6e9f3dbb0d2ef193ea483a195616ac2d096b0184
ashwani.kumar@nvish.com <ashwani.kumar@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-25 15:22:18 +0000

    Added Icon and Alt fields
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@960 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/slingmodels/LeaderShipBIODetailsModel.java
+++ bundle/src/main/java/com/hdscorp/cms/slingmodels/LeaderShipBIODetailsModel.java
@@ -17,18 +17,13 @@ public class LeaderShipBIODetailsModel {
 
 	@Inject
 	@Named("jcr:lbdimage")
-	@Default(values = "/content/dam/geometrixx-outdoors/logo.png")
+	@Default(values = "/content/dam/public/en_us/images/aboutus/leader-img.jpg")
 	private String lbdimage;
 
 	@Inject
 	@Named("jcr:lbdtitle")
 	@Default(values = "Title")
 	private String lbdTitle;
-
-	@Inject
-	@Named("jcr:lbdjoblocation")
-	@Default(values = "Job Location")
-	private String lbdJobLocation;
 
 	@Inject
 	@Named("jcr:lbdjobtitle")
@@ -54,8 +49,62 @@ public class LeaderShipBIODetailsModel {
 	@Named("jcr:lbdcontent")
 	@Default(values = "Content")
 	private String lbdContent;
+	
+	@Inject
+	@Named("jcr:lbdtwittericonpath")
+	@Default(values = "Twitter Icon Path")
+	private String lbdtwittericonpath;
+	
+	@Inject
+	@Named("jcr:lbdfacebookiconpath")
+	@Default(values = "Facebook Icon Path")	
+	private String lbdfacebookiconpath;
+	
+	@Inject
+	@Named("jcr:lbdlinkediniconpath")
+	@Default(values = "Linkedin Icon Path")	
+	private String lbdlinkediniconpath;
+	
+	@Inject
+	@Named("jcr:lbdtwitteralt")
+	@Default(values = "Twitter Alt")	
+	private String lbdtwitteralt;
+	
+	@Inject
+	@Named("jcr:lbdfacebookalt")
+	@Default(values = "Facebook Alt")	
+	private String lbdfacebookalt;
+	
+	@Inject
+	@Named("jcr:lbdlinkedinalt")
+	@Default(values = "Linkedin Alt")	
+	private String lbdlinkedinalt;
+	
+	
+	public String getLbdtwitteralt() {
+		return lbdtwitteralt;
+	}
 
+	public String getLbdfacebookalt() {
+		return lbdfacebookalt;
+	}
 
+	public String getLbdlinkedinalt() {
+		return lbdlinkedinalt;
+	}
+
+	public String getLbdtwittericonpath() {
+		return lbdtwittericonpath;
+	}
+
+	public String getLbdfacebookiconpath() {
+		return lbdfacebookiconpath;
+	}
+
+	public String getLbdlinkediniconpath() {
+		return lbdlinkediniconpath;
+	}
+	
 
 	public String getLbdimage() {
 		return lbdimage;
@@ -63,10 +112,6 @@ public class LeaderShipBIODetailsModel {
 
 	public String getLbdTitle() {
 		return lbdTitle;
-	}
-
-	public String getLbdJobLocation() {
-		return lbdJobLocation;
 	}
 
 	public String getLbdJobTitle() {
