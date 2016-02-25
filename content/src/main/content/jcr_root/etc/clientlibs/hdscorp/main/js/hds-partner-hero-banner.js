9182b8a1b912ddaf27f9108cf45b769f066f4bb3
pasam.venkataramana@hds.com <pasam.venkataramana@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-02-25 09:37:59 +0000

    Added hds-partner-hero-banner.js
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@931 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/hds-partner-hero-banner.js
@@ -0,0 +1,57 @@
+var hds = window.hds || {};
+
+(function(window, document, $, hds) {
+    hds.partnerHomeBanner = {
+        init: function(options) {
+            var defaults = {
+                element: '#partnerHeroBanner',
+                elementThumbs: '#partnerpageBar'
+            }
+            this.options = $.extend(defaults, options);
+            hds.partnerHomeBanner._initCarsoul();
+        },
+        _initCarsoul: function() {
+            var carousalContainer = this.options.element;
+            var thumbs = this.options.elementThumbs;
+            $(thumbs).find('a').each(function(i) {
+                $(this).addClass('itm' + i);
+                $(this).click(function() {
+                    $(carousalContainer).trigger('slideTo', [i, 0, true]);
+                    return false;
+                });
+            });
+            $(thumbs).find('a.itm0').addClass('active');
+            $(carousalContainer).carouFredSel({
+                auto: {
+                    fx: 'crossfade',
+                },
+                width: $(window).width(),
+                height: 470,
+                align: false,
+                items: {
+                    visible: 1,
+                    width: 'variable',
+                    height: 'variable'
+                },
+                scroll: {
+                    fx: 'crossfade',
+                    duration: 1500,
+                    onBefore: function() {
+                        var pos = $(this).triggerHandler('currentPosition');
+                        $(thumbs).find('a').removeClass('active');
+                        $(thumbs).find('a.itm' + pos).addClass('active');
+                        var page = Math.floor(pos / 3);
+                        $(thumbs).find('a').trigger('slideToPage', page);
+                    }
+                }
+
+            })
+        }
+    }
+}(window, document, jQuery, hds));
+
+$(function() {
+    if ($("#partnerCarsoul")) {
+        hds.partnerHomeBanner.init();
+    }
+})
