778d98a7c273b4f673e804f716a0f5e1cdc3a231
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-03 04:34:52 +0000

    debug servlet added
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1118 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ bundle/src/main/java/com/hdscorp/cms/servlet/ProductQueryServlet.java
@@ -0,0 +1,108 @@
+package com.hdscorp.cms.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.jcr.Session;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.jcr.resource.JcrResourceResolverFactory;
+
+import com.day.cq.search.result.SearchResult;
+import com.day.cq.tagging.JcrTagManagerFactory;
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.commons.ResourceIterator;
+import com.day.cq.wcm.foundation.List;
+import com.hdscorp.cms.search.SearchServiceHelper;
+import com.hdscorp.cms.util.ViewHelperUtil;
+
+@Component(immediate = true)
+@Service(Servlet.class)
+@Properties({
+		@Property(name = "service.description", value = { "Product Query" }),
+		@Property(name = "service.vendor", value = { "HDS Corp" }),
+		@Property(name = "sling.servlet.paths", value = { "/servicehdscorp/servlets/productquery" }),
+		@Property(name = "sling.servlet.methods", value = { "GET" }) })
+public class ProductQueryServlet extends SlingAllMethodsServlet {
+	private static final long serialVersionUID = -2128122335811219481L;
+
+	@Reference
+	private JcrResourceResolverFactory resolverFactory;
+	
+	@Reference
+	private SlingRepository repository;
+	
+	@Reference
+    JcrTagManagerFactory tmf;
+	
+	Session adminSession = null;
+	
+	@Override
+	public void doGet(SlingHttpServletRequest request,SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    
+	response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    
+    try {    
+    	adminSession = repository.loginAdministrative(null);
+        ResourceResolver resourceResolver = resolverFactory.getResourceResolver(adminSession);
+
+	    SearchServiceHelper searchServiceHelper = (SearchServiceHelper)ViewHelperUtil.getService(com.hdscorp.cms.search.SearchServiceHelper.class);
+		String paths[] = {"/content/hdscorp/en_us/products-solutions"};
+		String template= "/apps/hdscorp/templates/productdetail";
+		boolean doPagination = false;
+		String[] type = {"cq:Page"};
+		String tags[] =  {""};
+		
+		SearchResult result = searchServiceHelper.getFullTextBasedResuts(paths,null,template,null,null,doPagination,null,null,resourceResolver,null,null);
+		Iterator<Resource> reIT= result.getResources();
+		TagManager tMgr = tmf.getTagManager(adminSession);
+		
+		int cnt = 1 ; 
+		while (reIT.hasNext())
+		  {
+		    try {
+				Resource res = (Resource) reIT.next();		    
+				out.println(cnt+"- "+res.getName()+"-- ");
+				String[] pageTags= {""};
+				Page reourcePage = res.adaptTo(Page.class);
+				if(res!=null && reourcePage!=null && reourcePage.getProperties()!=null){
+					pageTags= (String[])reourcePage.getProperties().get("cq:tags");
+				}else{
+					
+				}
+				out.println(pageTags.length + "<br>");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    
+		    cnt++;
+		  }
+		
+		System.out.println("-----------------"+result.getHits().size());
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+  }
+
+}
