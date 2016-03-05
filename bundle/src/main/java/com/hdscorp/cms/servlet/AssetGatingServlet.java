3e385ecb95c9b71dedc44083a81ec8c6fa1a9db8
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-05 04:51:32 +0000

    gated servlet update
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1191 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/servlet/AssetGatingServlet.java
+++ bundle/src/main/java/com/hdscorp/cms/servlet/AssetGatingServlet.java
@@ -10,9 +10,13 @@ import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.request.RequestDispatcherOptions;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
-@SlingServlet(resourceTypes = { "dam:Asset" }, methods = { "GET" }, extensions = { "pdf" })
+import com.day.cq.dam.api.Asset;
+
+@SlingServlet(resourceTypes = { "dam:Asset" }, methods = { "GET" })
 @Properties({
 		@Property(name = "service.pid", value = "com.hdscorp.cms.servlet.AssetGatingServlet", propertyPrivate = false),
 		@Property(name = "service.description", value = "Asset Gating Servlet", propertyPrivate = false),
@@ -30,10 +34,34 @@ public class AssetGatingServlet extends SlingSafeMethodsServlet {
 		//5. If #4 conditions are met, check user has the GA cookie in the request.
 		//6. If no GA cookie found, then forward the request to the page which hosts the marketo form
 		//7. If GA cookie found make a call to the reg DB with the GA cookie and the PDF resource name
-		//8. If regdb responds that user has already registered, forward to the PDF resource 
-		options.setForceResourceType("dam/asset");
-		String forwardPath = "";
-		request.getRequestDispatcher(request.getResource(), options).forward(request, response);
+		//8. If regdb responds that user has already registered, forward to the PDF resource
+		String pdfPath= request.getRequestURI();
+		System.out.println("-----pdfPath---"+pdfPath);
+		try {
+			if(pdfPath.toLowerCase().contains(".pdf")){
+				ResourceResolver resourceResolver = request.getResourceResolver();
+				Resource res = resourceResolver.getResource(pdfPath);
+				Asset asset = res.adaptTo(Asset.class);
+				//If the resoure exists
+				if(asset!=null){
+					String resourceTitle = asset.getMetadataValue("dc:title");
+					//Get resource meta information and check if PDF has isGated property set to true and the date is within gated date range set on the pdf
+				}else{
+					options.setForceResourceType("dam/asset");
+				}
+				
+				
+				options.setForceResourceType("dam/asset");
+			}else{
+				options.setForceResourceType("dam/asset");
+				String forwardPath = "";			
+			}
+			request.getRequestDispatcher(request.getResource(), options).forward(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("-----In ERROR BLOCK------"+e.getMessage());
+		}
 		//9. If regdb responds that user has not registered, forward to the user to the marketo form (Should be a configurable property)
 				
 	}
