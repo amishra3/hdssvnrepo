3f1e49e5e99f7af25bad9f93a15106c0dcc0e601
sandeep.kaur@hds.com <sandeep.kaur@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-09 20:53:01 +0000

    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1306 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/gsa-search.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/gsa-search.js
@@ -13,37 +13,50 @@ var hds = window.hds || {};
              $(document).on('keypress', '#gsaSearchBox', function(event) {
             	var keycode = (event.keyCode ? event.keyCode : event.which);
             	if(keycode == 13) {
-	            	event.preventDefault();
+	            	event.preventDefault(); 
+                    if(!hds.gsasearch.validateSearchBox())
+                        return false;
 	                var q = $(this).val();
 					window.location=searchUrl+"?q="+q;
             	}
             });
-            $(document).on('click', '.icon nav-search', function(event) {
+            $(document).on('click', '.nav-search', function(event) {
                 //alert("..");
             	event.preventDefault();
+                 if(!hds.gsasearch.validateSearchBox())
+                        return false;
 	                var q = $('#gsaSearchBox').val();
-	               	var q = $(this).val();
-					window.location=searchUrl+"?q="+q;
+	               	window.location=searchUrl+"?q="+q;
             });
 
         },
         getQueryParameter: function(param) {
             var results = new RegExp('[\?&]' + param + '=([^&#]*)').exec(window.location.href);
-			return results[1] || 0;
-
-        }
-        ,
+            if(results!==null)
+				return results[1] || 0;
+            else
+                return "";
+        },
         setSearchIframe: function() {
 			var iframe = $('#searchFrame');
             var iframeUrl="http://wwwstage-revamp.hds.com/search?access=p&output=xml_no_dtd&client=hdsrevamp&proxystylesheet=hdsrevamp&sort=date:D:L:d1&oe=UTF-8&ie=UTF-8&ud=1&exclude_apps=1&site=hdsrevamp&q="+hds.gsasearch.getQueryParameter('q');
 			 iframe.attr('src', iframeUrl);
         }
+        ,
+        validateSearchBox: function() {
+            var q = $('#gsaSearchBox').val();
+            if(!q || q=="")
+                return false;
+            else
+                return true;
+
+
+        }
     }
 }(window, document, jQuery, hds));
 $(function() {
     hds.gsasearch.init();
-    if ( $('#searchFrame')){
-        console.log("*********************calling iframe code");
-		hds.gsasearch.setSearchIframe();
+    if ($('#searchFrame').length > 0){
+        hds.gsasearch.setSearchIframe();
 	}
 });
