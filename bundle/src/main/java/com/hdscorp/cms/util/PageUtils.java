912a82a3823c4d6c011c478c91535cd095e41e61
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-09 21:31:48 +0000

    added toMap and toList method
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1307 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/util/PageUtils.java
+++ bundle/src/main/java/com/hdscorp/cms/util/PageUtils.java
@@ -763,4 +763,42 @@ public static String  getComponentPath(final Page currentPage, ResourceResolver 
 		}
 		return 0;
 	}
+    
+    
+public static Map<String, Object> toMap(JSONObject object) throws JSONException, org.apache.sling.commons.json.JSONException {
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
+	    Iterator<String> keysItr = object.keys();
+	    while(keysItr.hasNext()) {
+	        String key = keysItr.next();
+	        Object value = object.get(key);
+
+	        if(value instanceof JSONArray) {
+	            value = toList((JSONArray) value);
+	        }
+
+	        else if(value instanceof JSONObject) {
+	            value = toMap((JSONObject) value);
+	        }
+	        map.put(key, value);
+	    }
+	    return map;
+	}
+
+	public static List<Object> toList(JSONArray array) throws JSONException, org.apache.sling.commons.json.JSONException {
+	    List<Object> list = new ArrayList<Object>();
+	    for(int i = 0; i < array.length(); i++) {
+	        Object value = array.get(i);
+	        if(value instanceof JSONArray) {
+	            value = toList((JSONArray) value);
+	        }
+
+	        else if(value instanceof JSONObject) {
+	            value = toMap((JSONObject) value);
+	        }
+	        list.add(value);
+	    }
+	    return list;
+	}
 }
