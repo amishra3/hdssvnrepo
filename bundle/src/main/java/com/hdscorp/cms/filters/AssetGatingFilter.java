160562c2ca656c79b5428448582d537c0499df0e
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-15 03:24:54 +0000

    Asset gating filter added and servlet disabled
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1465 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ bundle/src/main/java/com/hdscorp/cms/filters/AssetGatingFilter.java
@@ -0,0 +1,97 @@
+package com.hdscorp.cms.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.felix.scr.annotations.sling.SlingFilter;
+import org.apache.felix.scr.annotations.sling.SlingFilterScope;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.api.WCMMode;
+import com.hdscorp.cms.config.HdsCorpGlobalConfiguration;
+import com.hdscorp.cms.util.HdsCorpCommonUtils;
+
+@SlingFilter(
+        label = "HDS CORP Asset Gating Filter",
+        description = "HDS CORP Asset Gating Filter to check & handle gated assets",
+        metatype = false,
+        generateComponent = true, // True if you want to leverage activate/deactivate or manage its OSGi life-cycle
+        generateService = true, // True; required for Sling Filters
+        order = 0, // The smaller the number, the earlier in the Filter chain (can go negative);
+                    // Defaults to Integer.MAX_VALUE which push it at the end of the chain
+        scope = SlingFilterScope.REQUEST) // REQUEST, INCLUDE, FORWARD, ERROR, COMPONENT (REQUEST, INCLUDE, COMPONENT)
+public class AssetGatingFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(AssetGatingFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Usually, do nothing
+    }
+
+    @Override
+    public final void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+    	
+    	log.debug("===============Filter Called===============");
+    	final WCMMode mode = WCMMode.fromRequest(request);
+    	try {
+			
+			if (!(request instanceof SlingHttpServletRequest) || !(response instanceof SlingHttpServletResponse)) {
+				// Not a SlingHttpServletRequest/Response, so ignore.
+				log.debug("---------------Not a SlingHttpServletRequest/Response, so ignore-----------");
+				chain.doFilter(request, response);
+				return;
+			}
+			
+			final SlingHttpServletResponse slingResponse = (SlingHttpServletResponse) response;
+			final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+			String pdfPath= slingRequest.getRequestURI();
+			if(pdfPath.toLowerCase().endsWith(".pdf") && (pdfPath.startsWith("/en-us/pdf") || pdfPath.startsWith("/content/dam/public/en_us/pdfs"))){
+				log.debug("=========== pdfPath cleared the IF criteria for PDF - "+pdfPath+"===============");
+				String forwardPath = (String)HdsCorpGlobalConfiguration.getPropertyValue(HdsCorpGlobalConfiguration.ASSET_GATING_FORM_PATH);
+				String refererString = slingRequest.getHeader("Referer") ;
+				String gatingParam = (String)HdsCorpGlobalConfiguration.getPropertyValue(HdsCorpGlobalConfiguration.ASSET_GATING_SUCCESS_QUERY_PARAMETER);
+				String gatingParamVal = slingRequest.getParameter(gatingParam);				
+				if(HdsCorpCommonUtils.isGated(pdfPath, slingRequest) && !HdsCorpCommonUtils.checkValidReferer(refererString, gatingParamVal)){
+					log.debug("==========PDF is GATED ============");
+					String targetURL = forwardPath ;
+					slingRequest.setAttribute("pdfPath", pdfPath);
+					slingRequest.getRequestDispatcher(targetURL).forward(request,response);
+//					HttpServletResponse httpResponse = (HttpServletResponse) response;
+//					httpResponse.sendRedirect(PathResolver.getShortURLPath(targetURL));
+					return;
+				}else{
+					log.debug("==========Skipping the Filter==========");
+					chain.doFilter(request, response);
+					return;
+				}
+
+			}else{
+				log.debug("===============Skipping the Filter===============");
+				chain.doFilter(request, response);
+				return;				
+			}			
+    		
+		} catch (Exception ex) {
+			log.error("Asset Gating Filter Error Block" + ex.getMessage());
+		}
+
+        
+        // Finally, proceed with the the Filter chain
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // Usually, do Nothing
+    }
+}
