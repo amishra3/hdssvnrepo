fda4a2c6a9a788097912157c00129df8b27518b0
pasam.venkataramana@hds.com <pasam.venkataramana@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-15 17:21:46 +0000

    Added TWITTER_URL constant
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1493 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/constants/ServiceConstants.java
+++ bundle/src/main/java/com/hdscorp/cms/constants/ServiceConstants.java
@@ -134,7 +134,7 @@ public final class ServiceConstants {
 	public static final String TWITTER_MESSAGE_TEXT = "twitterMessageText";
 	public static final String TWITTER_MEDIA_URL = "twitterMediaUrl";
 	public static final String TWITTER_SAVE_FEED_DATA_PROPERTY_NAME = "twitterResponse";
-	
+	public static final String TWITTER_URL = "twitterURL";
 
 	/*
 	 * Facebook constraints
