181597dbfe1ee0817fbe1aae7a7e95f2011e315d
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-16 19:29:33 +0000

    Optimized code for Shorten API
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1526 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/restservice/ShortenerURLService.java
+++ bundle/src/main/java/com/hdscorp/cms/restservice/ShortenerURLService.java
@@ -1,28 +1,70 @@
 package com.hdscorp.cms.restservice;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Dictionary;
+
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.hdscorp.cms.constants.ServiceConstants;
 
 /**
  * This service returns short form of URL corresponding URLS
+ * 
  * @author gokula.nand
  *
  */
 @Component(immediate = true)
 @Service(value = ShortenerURLService.class)
-public class ShortenerURLService  extends GenericRestfulServiceInvokers{
-	  private static final Logger log = LoggerFactory.getLogger(ShortenerURLService.class);	
-	
-	public String getShortURL(String pageURL){
-		String shortURL="";
-		if(pageURL!=null && !pageURL.isEmpty()){		
-		shortURL=getWSResponse(ServiceConstants.SHORTEN_URL.concat(pageURL), ServiceConstants.GET_METHOD_TYPE, ServiceConstants.FEED_PARAMETER);
-		log.info("short url::"+shortURL);
+public class ShortenerURLService extends GenericRestfulServiceInvokers {
+	private static final Logger log = LoggerFactory.getLogger(ShortenerURLService.class);
+
+	@Reference
+	ConfigurationAdmin configurationAdmin;
+
+	public static final String SHORTEN_URL = "shorten.url";
+	public static final String SHORTEN_PID = "com.hdscorp.cms.restservice.ShortenerURLService";
+
+	public String getShortURL(String pageURL) {
+		String shortURL = "";
+		if (pageURL != null && !pageURL.isEmpty()) {
+			shortURL = getURLfromXML(getWSResponse(getShortURLPreviousPath().concat(pageURL),
+					ServiceConstants.GET_METHOD_TYPE, ServiceConstants.FEED_PARAMETER));
+			log.info("short url::" + shortURL);
 		}
 		return shortURL;
+	}
+
+	public String getURLfromXML(String xml) {
+		if (xml != null && !xml.isEmpty() && xml.indexOf("<shorturl>") != -1) {
+			xml = xml.substring(xml.indexOf("<shorturl>") + 10, xml.indexOf("</shorturl>"));
+		}
+		return xml;
+
+	}
+
+	private String getShortURLPreviousPath() {
+		Configuration config;
+		String locationPath = "";
+		try {
+			config = configurationAdmin.getConfiguration(SHORTEN_PID);
+			Dictionary props = config.getProperties();
+			locationPath = (String) config.getProperties().get(SHORTEN_URL);
+			if (locationPath != null && !locationPath.isEmpty()) {
+				locationPath = ServiceConstants.SHORTEN_URL;
+			}
+		} catch (Exception e) {
+			StringWriter stack = new StringWriter();
+			e.printStackTrace(new PrintWriter(stack));
+			log.error("Error occurs while getting location path:  " + stack.toString());
+		}
+		return locationPath;
 	}
 
 }
