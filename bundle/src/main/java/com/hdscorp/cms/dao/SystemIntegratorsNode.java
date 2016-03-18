55be40420e86d4b2b9d8fdf8bd01e82006d98417
pasam.venkataramana@hds.com <pasam.venkataramana@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-18 11:05:16 +0000

    Added SystemIntegratorsNode.java file
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1568 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ bundle/src/main/java/com/hdscorp/cms/dao/SystemIntegratorsNode.java
@@ -0,0 +1,122 @@
+package com.hdscorp.cms.dao;
+
+import java.util.ArrayList;
+
+public class SystemIntegratorsNode {
+   
+
+	private String partnerTitle;
+	
+	private String partnerPath;
+	
+	private String partnerDescription;
+	
+	private String partnerBackgroundImagePath;
+
+	private String partnerIconImagePath;
+	
+	private String partnerIconImageAltText;
+
+	private String partnerName;
+	
+	private String partnerHeadLine;
+	
+	private String partnerIntroduction;
+	
+	private String[] partnerTags;
+	
+	private ArrayList<PartnerDescription> descriptionList;
+
+	
+	public String getPartnerTitle() {
+		return partnerTitle;
+	}
+
+	public void setPartnerTitle(String partnerTitle) {
+		this.partnerTitle = partnerTitle;
+	}
+
+	public String getPartnerPath() {
+		return partnerPath;
+	}
+
+	public void setPartnerPath(String partnerPath) {
+		this.partnerPath = partnerPath;
+	}
+
+	
+	public String getPartnerBackgroundImagePath() {
+		return partnerBackgroundImagePath;
+	}
+
+	public void setPartnerBackgroundImagePath(String partnerBackgroundImagePath) {
+		this.partnerBackgroundImagePath = partnerBackgroundImagePath;
+	}
+
+	public String getPartnerIconImagePath() {
+		return partnerIconImagePath;
+	}
+
+	public void setPartnerIconImagePath(String partnerIconImagePath) {
+		this.partnerIconImagePath = partnerIconImagePath;
+	}
+
+	public String getPartnerIconImageAltText() {
+		return partnerIconImageAltText;
+	}
+
+	public void setPartnerIconImageAltText(String partnerIconImageAltText) {
+		this.partnerIconImageAltText = partnerIconImageAltText;
+	}
+
+	
+	public ArrayList<PartnerDescription> getDescriptionList() {
+		return descriptionList;
+	}
+
+	public void setDescriptionList(ArrayList<PartnerDescription> descriptionList) {
+		this.descriptionList = descriptionList;
+	}
+
+	public String[] getPartnerTags() {
+		return partnerTags;
+	}
+
+	public void setPartnerTags(String[] partnerTags) {
+		this.partnerTags = partnerTags;
+	}
+
+
+	public String getPartnerDescription() {
+		return partnerDescription;
+	}
+
+	public void setPartnerDescription(String partnerDescription) {
+		this.partnerDescription = partnerDescription;
+	}
+
+	public String getPartnerName() {
+		return partnerName;
+	}
+
+	public void setPartnerName(String partnerName) {
+		this.partnerName = partnerName;
+	}
+
+	public String getPartnerHeadLine() {
+		return partnerHeadLine;
+	}
+
+	public void setPartnerHeadLine(String partnerHeadLine) {
+		this.partnerHeadLine = partnerHeadLine;
+	}
+
+	public String getPartnerIntroduction() {
+		return partnerIntroduction;
+	}
+
+	public void setPartnerIntroduction(String partnerIntroduction) {
+		this.partnerIntroduction = partnerIntroduction;
+	}
+	
+}
