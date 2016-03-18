838bb8c2a8737e896713dc02b10a3f144e64d77b
pasam.venkataramana@hds.com <pasam.venkataramana@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-18 11:01:45 +0000

    Added type News and Resource
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1567 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/slingmodels/NewsVerticalExplorerModel.java
+++ bundle/src/main/java/com/hdscorp/cms/slingmodels/NewsVerticalExplorerModel.java
@@ -13,8 +13,10 @@ import org.apache.sling.models.annotations.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.day.cq.dam.api.Asset;
 import com.hdscorp.cms.constants.ServiceConstants;
 import com.hdscorp.cms.dao.NewsInsightExplorerModel;
+import com.hdscorp.cms.util.HdsCorpCommonUtils;
 import com.hdscorp.cms.util.JcrUtilService;
 import com.hdscorp.cms.util.ServiceUtil;
 
@@ -27,6 +29,9 @@ import com.hdscorp.cms.util.ServiceUtil;
 @Model(adaptables = Resource.class)
 public class NewsVerticalExplorerModel {
 	private static final Logger log = LoggerFactory.getLogger(NewsVerticalExplorerModel.class);
+	
+	private final String FROM_DATE = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
+	private final String TO_DATE = "MMMM yyyy";
 
 	@Inject
 	@Named("nveiconbimagepath")
@@ -123,6 +128,60 @@ public class NewsVerticalExplorerModel {
 				}
 
 			}
+		}
+		
+		else if (getEventType().equalsIgnoreCase("News")) {
+
+			newsInsightExplorer = new NewsInsightExplorerModel();
+			resource = JcrUtilService.getResourceResolver().resolve(getTargetURL() + "/jcr:content/newsdetail");
+			if (!resource.isResourceType(Resource.RESOURCE_TYPE_NON_EXISTING)) {
+				ValueMap properties = resource.adaptTo(ValueMap.class);
+				newsInsightExplorer.setTitle(properties.get("newstitle", (String) null).toString());
+				Calendar cal = (Calendar) properties.get("newsdate");
+				try {
+					newsInsightExplorer.setPubDate(ServiceUtil.getStringFromDate(cal.getTime(),
+							ServiceConstants.DATE_FORMAT_TO_FULL_MONTH_YEAR));
+				} catch (ParseException e) {
+					log.info("Exception occurs duing getting value from Node: " + e);
+				}
+
+			}
+		}
+
+		else if (getEventType().equalsIgnoreCase("Resource")) {
+
+			newsInsightExplorer = new NewsInsightExplorerModel();
+
+			String pdfPath = HdsCorpCommonUtils.pdfJCRPath(getTargetURL());
+
+			resource = JcrUtilService.getResourceResolver().resolve(pdfPath);
+
+			if (!resource.isResourceType(Resource.RESOURCE_TYPE_NON_EXISTING)) {
+
+				Asset asset = resource.adaptTo(Asset.class);
+
+				if (asset != null) {
+					newsInsightExplorer.setTitle(asset.getMetadataValue("dc:title") != null
+							? asset.getMetadataValue("dc:title").toString() : "");
+					try {
+						if (asset.getMetadataValue("dc:creationdate") != null
+								&& !asset.getMetadataValue("dc:creationdate").trim().isEmpty()) {
+
+							newsInsightExplorer.setPubDate(ServiceUtil.getDisplayDateFormat(
+									asset.getMetadataValue("dc:creationdate").toString(), FROM_DATE, TO_DATE));
+
+						} else {
+
+							newsInsightExplorer.setPubDate(ServiceUtil.getDisplayDateFormat(
+									asset.getMetadataValue("xmp:CreateDate").toString(), FROM_DATE, TO_DATE));
+							log.info("padf created date " + asset.getMetadataValue("xmp:CreateDate").toString());
+						}
+					} catch (Exception e) {
+						log.info("Exception occurs duing getting value from Node: " + e);
+					}
+				}
+			}
+
 		}
 
 		return newsInsightExplorer;
