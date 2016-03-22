5973490120363484ad6c5eea35e80ac8244f4957
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-22 07:59:46 +0000

    remove tested code from
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1651 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/dao/SystemIntegratorsNode.java
+++ bundle/src/main/java/com/hdscorp/cms/dao/SystemIntegratorsNode.java
@@ -5,129 +5,122 @@ import java.util.List;
 import java.util.Map;
 
 public class SystemIntegratorsNode {
-   
 
-	private String partnerTitle;
-	
-	private String partnerPath;
-	
-	private String partnerDescription;
-	
-	private String partnerBackgroundImagePath;
+	private String systemIntegratorTitle;
 
-	private String partnerIconImagePath;
-	
-	private String partnerIconImageAltText;
+	private String systemIntegratorPath;
 
-	private String partnerName;
-	
-	private String partnerHeadLine;
-	
-	private String partnerIntroduction;
-	
-	private String[] partnerTags;
-	
-	private String[] siteTags;
-	
-	private List<Map<String, Object>> listmap;
-	
-	
+	private String systemIntegratorDescription;
+
+	private String systemIntegratorBackgroundImagePath;
+
+	private String systemIntegratorIconImagePath;
+
+	private String systemIntegratorIconImageAltText;
+
+	private String systemIntegratorName;
+
+	private String systemIntegratorHeadLine;
+
+	private String systemIntegratorIntroduction;
+
+	private String[] systemIntegratorTags;
+
+	private List<String> industryTadIds;
+
 	private List<Map<String, String>> contentCell;
-	
-	
+
 	private ArrayList<PartnerDescription> descriptionList;
 
+	public String getSystemIntegratorTitle() {
+		return systemIntegratorTitle;
+	}
+
+	public void setSystemIntegratorTitle(String systemIntegratorTitle) {
+		this.systemIntegratorTitle = systemIntegratorTitle;
+	}
+
+	public String getSystemIntegratorPath() {
+		return systemIntegratorPath;
+	}
+
+	public void setSystemIntegratorPath(String systemIntegratorPath) {
+		this.systemIntegratorPath = systemIntegratorPath;
+	}
+
+	public String getSystemIntegratorDescription() {
+		return systemIntegratorDescription;
+	}
+
+	public void setSystemIntegratorDescription(String systemIntegratorDescription) {
+		this.systemIntegratorDescription = systemIntegratorDescription;
+	}
+
+	public String getSystemIntegratorBackgroundImagePath() {
+		return systemIntegratorBackgroundImagePath;
+	}
+
+	public void setSystemIntegratorBackgroundImagePath(String systemIntegratorBackgroundImagePath) {
+		this.systemIntegratorBackgroundImagePath = systemIntegratorBackgroundImagePath;
+	}
+
+	public String getSystemIntegratorIconImagePath() {
+		return systemIntegratorIconImagePath;
+	}
+
+	public void setSystemIntegratorIconImagePath(String systemIntegratorIconImagePath) {
+		this.systemIntegratorIconImagePath = systemIntegratorIconImagePath;
+	}
+
+	public String getSystemIntegratorIconImageAltText() {
+		return systemIntegratorIconImageAltText;
+	}
+
+	public void setSystemIntegratorIconImageAltText(String systemIntegratorIconImageAltText) {
+		this.systemIntegratorIconImageAltText = systemIntegratorIconImageAltText;
+	}
+
+	public String getSystemIntegratorName() {
+		return systemIntegratorName;
+	}
+
+	public void setSystemIntegratorName(String systemIntegratorName) {
+		this.systemIntegratorName = systemIntegratorName;
+	}
+
+	public String getSystemIntegratorHeadLine() {
+		return systemIntegratorHeadLine;
+	}
+
+	public void setSystemIntegratorHeadLine(String systemIntegratorHeadLine) {
+		this.systemIntegratorHeadLine = systemIntegratorHeadLine;
+	}
+
+	public String getSystemIntegratorIntroduction() {
+		return systemIntegratorIntroduction;
+	}
+
+	public void setSystemIntegratorIntroduction(String systemIntegratorIntroduction) {
+		this.systemIntegratorIntroduction = systemIntegratorIntroduction;
+	}
+
+	public String[] getSystemIntegratorTags() {
+		return systemIntegratorTags;
+	}
+
+	public void setSystemIntegratorTags(String[] systemIntegratorTags) {
+		this.systemIntegratorTags = systemIntegratorTags;
+	}
+
+	public List<String> getIndustryTadIds() {
+		return industryTadIds;
+	}
+
+	public void setIndustryTadIds(List<String> industryTadIds) {
+		this.industryTadIds = industryTadIds;
+	}
+
 	
-	public String getPartnerTitle() {
-		return partnerTitle;
-	}
-
-	public void setPartnerTitle(String partnerTitle) {
-		this.partnerTitle = partnerTitle;
-	}
-
-	public String getPartnerPath() {
-		return partnerPath;
-	}
-
-	public void setPartnerPath(String partnerPath) {
-		this.partnerPath = partnerPath;
-	}
-
-	
-	public String getPartnerBackgroundImagePath() {
-		return partnerBackgroundImagePath;
-	}
-
-	public void setPartnerBackgroundImagePath(String partnerBackgroundImagePath) {
-		this.partnerBackgroundImagePath = partnerBackgroundImagePath;
-	}
-
-	public String getPartnerIconImagePath() {
-		return partnerIconImagePath;
-	}
-
-	public void setPartnerIconImagePath(String partnerIconImagePath) {
-		this.partnerIconImagePath = partnerIconImagePath;
-	}
-
-	public String getPartnerIconImageAltText() {
-		return partnerIconImageAltText;
-	}
-
-	public void setPartnerIconImageAltText(String partnerIconImageAltText) {
-		this.partnerIconImageAltText = partnerIconImageAltText;
-	}
-
-	
-	public ArrayList<PartnerDescription> getDescriptionList() {
-		return descriptionList;
-	}
-
-	public void setDescriptionList(ArrayList<PartnerDescription> descriptionList) {
-		this.descriptionList = descriptionList;
-	}
-
-	public String[] getPartnerTags() {
-		return partnerTags;
-	}
-
-	public void setPartnerTags(String[] partnerTags) {
-		this.partnerTags = partnerTags;
-	}
-
-
-	public String getPartnerDescription() {
-		return partnerDescription;
-	}
-
-	public void setPartnerDescription(String partnerDescription) {
-		this.partnerDescription = partnerDescription;
-	}
-
-	public String getPartnerName() {
-		return partnerName;
-	}
-
-	public void setPartnerName(String partnerName) {
-		this.partnerName = partnerName;
-	}
-
-	public String getPartnerHeadLine() {
-		return partnerHeadLine;
-	}
-
-	public void setPartnerHeadLine(String partnerHeadLine) {
-		this.partnerHeadLine = partnerHeadLine;
-	}
-
-	public String getPartnerIntroduction() {
-		return partnerIntroduction;
-	}
-
-	public void setPartnerIntroduction(String partnerIntroduction) {
-		this.partnerIntroduction = partnerIntroduction;
-	}
 
 	public List<Map<String, String>> getContentCell() {
 		return contentCell;
@@ -137,24 +130,12 @@ public class SystemIntegratorsNode {
 		this.contentCell = contentCell;
 	}
 
-	public String[] getSiteTags() {
-		return siteTags;
+	public ArrayList<PartnerDescription> getDescriptionList() {
+		return descriptionList;
 	}
 
-	public void setSiteTags(String[] siteTags) {
-		this.siteTags = siteTags;
+	public void setDescriptionList(ArrayList<PartnerDescription> descriptionList) {
+		this.descriptionList = descriptionList;
 	}
 
-
-	public List<Map<String, Object>> getListmap() {
-		return listmap;
-	}
-
-	public void setListmap(List<Map<String, Object>> listmap) {
-		this.listmap = listmap;
-	}
-	
-	
-	
-	
 }
