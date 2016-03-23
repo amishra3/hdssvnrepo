84ed6a1a3322260cb4e4d34909bdcaf3a5ebdb8b
gokula.nand@nvish.com <gokula.nand@nvish.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-23 13:56:11 +0000

    Excluded terms which has category is Zero
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1712 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/restservice/BrightTalkWebService.java
+++ bundle/src/main/java/com/hdscorp/cms/restservice/BrightTalkWebService.java
@@ -45,7 +45,7 @@ public class BrightTalkWebService extends GenericRestfulServiceInvokers {
 	}
 
 	public String getBrightTalkFeedData(String xmlFeed) {
-		log.info("xml feed response"+xmlFeed);
+		log.info("xml feed response" + xmlFeed);
 
 		JSONArray feedList = new JSONArray();
 		try {
@@ -55,48 +55,27 @@ public class BrightTalkWebService extends GenericRestfulServiceInvokers {
 			NodeList nList = document.getElementsByTagName(ServiceConstants.ENTRY_TAG);
 
 			JSONObject feed = null;
+			boolean term = false;
 			for (int nCount = 0; nCount < nList.getLength(); nCount++) {
 
 				Node nNode = nList.item(nCount);
 
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					term = false;
 
 					Element eElement = (Element) nNode;
 
 					feed = new JSONObject();
-					feed.put(ServiceConstants.JSON_UPDATED_DATE, formatDate(
-							eElement.getElementsByTagName(ServiceConstants.UPDATED_DATE).item(0).getTextContent()));
-					feed.put(ServiceConstants.JSON_TITLE,
-							eElement.getElementsByTagName(ServiceConstants.TITLE).item(0).getTextContent());
-					feed.put(ServiceConstants.JSON_AUTHOR,
-							eElement.getElementsByTagName(ServiceConstants.AUTHOR).item(0).getTextContent());
-					feed.put(ServiceConstants.JSON_SUMMARY,
-							eElement.getElementsByTagName(ServiceConstants.SUMMARY).item(0).getTextContent());
-					if (eElement.getElementsByTagName(ServiceConstants.FEATURED).item(0) != null) {
-						feed.put(ServiceConstants.JSON_FEATURED,
-								eElement.getElementsByTagName(ServiceConstants.FEATURED).item(0).getTextContent());
-					} else {
-						feed.put(ServiceConstants.JSON_FEATURED, "");
-					}
-					feed.put(ServiceConstants.JSON_STATUS,
-							eElement.getElementsByTagName(ServiceConstants.STATUS).item(0).getTextContent());
-					feed.put(ServiceConstants.JSON_FORMAT,
-							eElement.getElementsByTagName(ServiceConstants.FORMAT).item(0).getTextContent());
-					feed.put(
-							ServiceConstants.JSON_DURATION, String
-									.valueOf(Integer.parseInt(eElement.getElementsByTagName(ServiceConstants.DURATION)
-											.item(0).getTextContent()) / ServiceConstants.SECOND)
-							.concat(ServiceConstants.MIN));
-					feed.put(ServiceConstants.JSON_START,
-							eElement.getElementsByTagName(ServiceConstants.START).item(0).getTextContent());
-					feed.put(ServiceConstants.JSON_RATING,
-							eElement.getElementsByTagName(ServiceConstants.RATING).item(0).getTextContent());
 
 					NodeList categoriesList = eElement.getElementsByTagName(ServiceConstants.CATEGORY);
 					StringBuffer categoryBuffer = new StringBuffer();
 					for (int catCount = 0; catCount < categoriesList.getLength(); catCount++) {
 						Node nNodeHref = categoriesList.item(catCount);
 						Element eElementHref = (Element) nNodeHref;
+						if (eElementHref.getAttribute(ServiceConstants.TERM).toString().equalsIgnoreCase("0")) {
+							term = true;
+						}
+
 						if (catCount == 0)
 							categoryBuffer.append(eElementHref.getAttribute(ServiceConstants.TERM).toString()
 									.replaceAll(ServiceConstants.REG_EXP, ""));
@@ -104,37 +83,69 @@ public class BrightTalkWebService extends GenericRestfulServiceInvokers {
 							categoryBuffer.append("," + eElementHref.getAttribute(ServiceConstants.TERM).toString()
 									.replaceAll(ServiceConstants.REG_EXP, ""));
 					}
-					feed.put(ServiceConstants.JSON_CATEGORY, categoryBuffer.toString());
-					NodeList nCommunication = eElement.getElementsByTagName(ServiceConstants.COMMUNICATION);
-					Node nNodeCommunication = nCommunication.item(0);
-					Element eElementCommunication = (Element) nNodeCommunication;
-					feed.put(ServiceConstants.JSON_COMMUNICATION_ID,
-							eElementCommunication.getAttribute(ServiceConstants.ID));
 
-					NodeList nChannel = eElement.getElementsByTagName(ServiceConstants.CHANNEL);
-					Node nNodeChannel = nChannel.item(0);
-					Element eElementChannel = (Element) nNodeChannel;
-					feed.put(ServiceConstants.JSON_CHANNEL_ID, eElementChannel.getAttribute(ServiceConstants.ID));
+					if (!term) {
 
-					NodeList hrefLinks = eElement.getElementsByTagName(ServiceConstants.LINK);
-					for (int hCount = 0; hCount < hrefLinks.getLength(); hCount++) {
-						Node nNodeHref = hrefLinks.item(hCount);
-						Element eElementHref = (Element) nNodeHref;
-						if (hCount == 0) {
-							feed.put(ServiceConstants.JSON_HERF_LINK, eElementHref.getAttribute(ServiceConstants.HREF));
-						} else if (hCount == 1) {
-							feed.put(ServiceConstants.JSON_THUMBNAIL_PATH,
-									eElementHref.getAttribute(ServiceConstants.HREF));
-						} else if (hCount ==2) {
-							feed.put(ServiceConstants.JSON_PREVIEW_IMAGE_PATH,
-									eElementHref.getAttribute(ServiceConstants.HREF));
+						feed.put(ServiceConstants.JSON_CATEGORY, categoryBuffer.toString());
+
+						feed.put(ServiceConstants.JSON_UPDATED_DATE, formatDate(
+								eElement.getElementsByTagName(ServiceConstants.UPDATED_DATE).item(0).getTextContent()));
+						feed.put(ServiceConstants.JSON_TITLE,
+								eElement.getElementsByTagName(ServiceConstants.TITLE).item(0).getTextContent());
+						feed.put(ServiceConstants.JSON_AUTHOR,
+								eElement.getElementsByTagName(ServiceConstants.AUTHOR).item(0).getTextContent());
+						feed.put(ServiceConstants.JSON_SUMMARY,
+								eElement.getElementsByTagName(ServiceConstants.SUMMARY).item(0).getTextContent());
+						if (eElement.getElementsByTagName(ServiceConstants.FEATURED).item(0) != null) {
+							feed.put(ServiceConstants.JSON_FEATURED,
+									eElement.getElementsByTagName(ServiceConstants.FEATURED).item(0).getTextContent());
+						} else {
+							feed.put(ServiceConstants.JSON_FEATURED, "");
 						}
+						feed.put(ServiceConstants.JSON_STATUS,
+								eElement.getElementsByTagName(ServiceConstants.STATUS).item(0).getTextContent());
+						feed.put(ServiceConstants.JSON_FORMAT,
+								eElement.getElementsByTagName(ServiceConstants.FORMAT).item(0).getTextContent());
+						feed.put(ServiceConstants.JSON_DURATION,
+								String.valueOf(Integer.parseInt(eElement.getElementsByTagName(ServiceConstants.DURATION)
+										.item(0).getTextContent()) / ServiceConstants.SECOND)
+								.concat(ServiceConstants.MIN));
+						feed.put(ServiceConstants.JSON_START,
+								eElement.getElementsByTagName(ServiceConstants.START).item(0).getTextContent());
+						feed.put(ServiceConstants.JSON_RATING,
+								eElement.getElementsByTagName(ServiceConstants.RATING).item(0).getTextContent());
 
+						NodeList nCommunication = eElement.getElementsByTagName(ServiceConstants.COMMUNICATION);
+						Node nNodeCommunication = nCommunication.item(0);
+						Element eElementCommunication = (Element) nNodeCommunication;
+						feed.put(ServiceConstants.JSON_COMMUNICATION_ID,
+								eElementCommunication.getAttribute(ServiceConstants.ID));
+
+						NodeList nChannel = eElement.getElementsByTagName(ServiceConstants.CHANNEL);
+						Node nNodeChannel = nChannel.item(0);
+						Element eElementChannel = (Element) nNodeChannel;
+						feed.put(ServiceConstants.JSON_CHANNEL_ID, eElementChannel.getAttribute(ServiceConstants.ID));
+
+						NodeList hrefLinks = eElement.getElementsByTagName(ServiceConstants.LINK);
+						for (int hCount = 0; hCount < hrefLinks.getLength(); hCount++) {
+							Node nNodeHref = hrefLinks.item(hCount);
+							Element eElementHref = (Element) nNodeHref;
+							if (hCount == 0) {
+								feed.put(ServiceConstants.JSON_HERF_LINK,
+										eElementHref.getAttribute(ServiceConstants.HREF));
+							} else if (hCount == 1) {
+								feed.put(ServiceConstants.JSON_THUMBNAIL_PATH,
+										eElementHref.getAttribute(ServiceConstants.HREF));
+							} else if (hCount == 2) {
+								feed.put(ServiceConstants.JSON_PREVIEW_IMAGE_PATH,
+										eElementHref.getAttribute(ServiceConstants.HREF));
+							}
+
+						}
+						feedList.put(feed);
 					}
-
 				}
-				feedList.put(feed);
-
+				
 			}
 
 		} catch (SAXParseException e) {
@@ -153,6 +164,13 @@ public class BrightTalkWebService extends GenericRestfulServiceInvokers {
 		log.info("End execution of getInvoke()");
 		return feedList.toString();
 
+	}
+	
+	public static void main(String args[]){
+		
+		BrightTalkWebService bb=new BrightTalkWebService();
+		System.out.println(bb.getBrightTalkResponse("https://www.brighttalk.com/channel/12821/feed"));
+		
 	}
 
 }
