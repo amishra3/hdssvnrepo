f1f0ed0c40dd30b0abfc41615b528ae0917be5c4
harpinder.singh@hds.com <harpinder.singh@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-25 19:19:43 +0000

    fixed height issues of Advantages section and spacing of Resources and community section.
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1782 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/product-solutions.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/product-solutions.js
@@ -191,26 +191,26 @@ if($('.fb-category-container').length!==0){
 				this.id = 'fixedRate' + index;
 
 				var callheightinner = 0;
-				for(var i=0;i<$("#fixedRate"+index+" .fb-category-points-box-description").size();i++){
-					if($("#fixedRate"+index+" .fb-category-points-box-description:eq("+i+")").height()>=callheightinner){
-							callheightinner=$("#fixedRate"+index+" .fb-category-points-box-description:eq("+i+")").height();
+				for(var i=0;i<$("#fixedRate"+index+" .fb-category-points-box").size();i++){
+					if($("#fixedRate"+index+" .fb-category-points-box:eq("+i+")").height()>=callheightinner){
+							callheightinner=$("#fixedRate"+index+" .fb-category-points-box:eq("+i+")").height();
 						}
 				}
-				$("#fixedRate"+index+" .fb-category-points-box-description").height(callheightinner);
+				$("#fixedRate"+index+" .fb-category-points-box").height(callheightinner);
 				
-				var callheightinnerhead=0;
+				/*var callheightinnerhead=0;
 				for(var i=0;i<$("#fixedRate"+index+" .fb-category-points-box-heading").size();i++){
 					if($("#fixedRate"+index+" .fb-category-points-box-heading:eq("+i+")").height()>=callheightinnerhead){
 							callheightinnerhead=$("#fixedRate"+index+" .fb-category-points-box-heading:eq("+i+")").height();
 						}
 				}
-				$("#fixedRate"+index+" .fb-category-points-box-heading").height(callheightinnerhead);
+				$("#fixedRate"+index+" .fb-category-points-box-heading").height(callheightinnerhead);*/
 			});
 
 }
 }
 
-if( $(window).width() > 1209){
+/*if( $(window).width() > 1209){
 if($('.mes-section').length!==0){
 
 				var resheightinner = 0;
@@ -221,13 +221,13 @@ if($('.mes-section').length!==0){
 				}
 				$(".product-box .product-copy-main").height(resheightinner);
 				
-				/*var resheightinnerhead=0;
+				var resheightinnerhead=0;
 				for(var i=0;i<$(".product-box .product-copy-sub").size();i++){
 					if($(".product-box .product-copy-sub:eq("+i+")").height()>=resheightinnerhead){
 							resheightinnerhead=$(".product-box .product-copy-sub:eq("+i+")").height();
 						}
 				}
-				$(".product-box .product-copy-sub").height(resheightinnerhead);*/
+				$(".product-box .product-copy-sub").height(resheightinnerhead);
 }
 }
 
@@ -242,15 +242,15 @@ if($('.resources-section').length!==0){
 				}
 				$(".resources-category-box .resources-category-title").height(setheightinner);
 				
-				/*var setheightinnerhead=0;
+				var setheightinnerhead=0;
 				for(var i=0;i<$(".resources-category-box .resources-category-description").size();i++){
 					if($(".resources-category-box .resources-category-description:eq("+i+")").height()>=setheightinnerhead){
 							setheightinnerhead=$(".resources-category-box .resources-category-description:eq("+i+")").height();
 						}
 				}
-				$(".resources-category-box .resources-category-description").height(setheightinnerhead);*/
+				$(".resources-category-box .resources-category-description").height(setheightinnerhead);
 }
-}
+}*/
 
 
 
