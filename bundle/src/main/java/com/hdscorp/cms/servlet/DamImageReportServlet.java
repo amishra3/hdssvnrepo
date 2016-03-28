11ee79d2e9dc97bded0e64548c050a796f52e254
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-28 02:20:45 +0000

    asset size servlet added
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1853 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

---
+++ bundle/src/main/java/com/hdscorp/cms/servlet/DamImageReportServlet.java
@@ -0,0 +1,172 @@
+package com.hdscorp.cms.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+import javax.jcr.query.Row;
+import javax.jcr.query.RowIterator;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.jcr.resource.JcrResourceResolverFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.dam.api.Asset;
+import com.day.cq.search.result.SearchResult;
+import com.day.cq.tagging.JcrTagManagerFactory;
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.commons.ResourceIterator;
+import com.day.cq.wcm.foundation.List;
+import com.day.cq.wcm.offline.HtmlUtil;
+import com.hdscorp.cms.search.SearchServiceHelper;
+import com.hdscorp.cms.util.ViewHelperUtil;
+
+@Component(immediate = true)
+@Service(Servlet.class)
+@Properties({
+		@Property(name = "service.description", value = { "Product Query" }),
+		@Property(name = "service.vendor", value = { "HDS Corp" }),
+		@Property(name = "sling.servlet.paths", value = { "/servicehdscorp/servlets/damimagereport" }),
+		@Property(name = "sling.servlet.methods", value = { "GET" }) })
+public class DamImageReportServlet extends SlingAllMethodsServlet {
+
+	private static final long serialVersionUID = -2128122335811219481L;
+	
+	@Reference
+	private SlingRepository repository;
+	
+	Session adminSession = null;
+	
+	@Override
+	public void doGet(SlingHttpServletRequest request,SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    
+	PrintWriter out = response.getWriter();
+	
+	final Logger LOG =  LoggerFactory.getLogger(DamImageReportServlet.class);
+	response.setContentType("text/html");
+	
+    try {
+    	
+    	String searchPath = request.getParameter("servicepath");
+    	String sizeparam = request.getParameter("size");
+    	int size = 0;
+    	if(sizeparam!=null && StringUtils.isNumeric(sizeparam)){
+    		size = Integer.parseInt(request.getParameter("size"));	
+    	}
+    	
+    	String defaultPath = "/content/dam/public/en_us/images/";
+    	int defaultSize = 150;
+    	
+    	if(searchPath==null || searchPath.trim().length()==0){
+    		searchPath = defaultPath ;
+    	}
+
+    	if(size==0){
+    		size = defaultSize ;
+    	}
+
+    	
+    	adminSession = repository.loginAdministrative(null);
+    	int resultCnt=0;
+        ResourceResolver resourceResolver = request.getResourceResolver();
+		
+		String sqlStatement= "select * from [nt:base] as p where (isdescendantnode (p, '"+searchPath+"')) and p.[jcr:primaryType] = 'dam:Asset'";
+		QueryManager queryManager = adminSession.getWorkspace().getQueryManager();
+		Query query = queryManager.createQuery(sqlStatement, Query.SQL);
+		
+		
+		try {
+			QueryResult result = query.execute();
+			RowIterator rowIterator = result.getRows();
+			out.println("<table border=1>");
+			
+			out.println("<th>");
+			out.println("");
+			out.println("</th>");
+
+			out.println("<th>");
+			out.println("ASSET PATH");
+			out.println("</th>");
+			
+			out.println("<th>");
+			out.println("FILE SIZE");
+			out.println(" KB </th>");
+			
+			out.println("<td>");
+			out.println("FILE TYPE");
+			out.println("</th>");
+
+			
+			while(rowIterator.hasNext()){
+				
+				Row row = rowIterator.nextRow();
+				Resource res = resourceResolver.getResource(row.getPath());
+				Asset asset = res.adaptTo(Asset.class);
+				String fileType = asset.getOriginal().getMimeType();
+				if(fileType!=null && fileType.contains("image")){
+					long filseSize = asset.getOriginal().getSize() / 1024;
+					if(filseSize > size){
+						resultCnt++;
+						out.println("<tr>");
+							out.println("<td>");
+							out.println(resultCnt);
+							out.println("</td>");
+	
+							out.println("<td>");
+							out.println(row.getPath());
+							out.println("</td>");
+							
+							out.println("<td>");
+							out.println(filseSize);
+							out.println(" KB </td>");
+							
+							out.println("<td>");
+							out.println(fileType);
+							out.println("</td>");
+						out.println("</tr>");
+					}
+				}
+			}
+			out.println("</table>");
+			
+		} catch (Exception e) {
+			LOG.error(e.getMessage());
+		}finally{
+			
+		}
+		
+		out.println("<span style='color:red;font-size:1.5em;font-weight:600;'>TOTLE IMAGES GREATER THAN "+size+" KB are"+"- "+resultCnt+"</span><br>");
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+  }
+
+}
