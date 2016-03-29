ee9a9ddaa2c5791718d43d8992da9ffb4771efa2
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-29 14:39:06 +0000

    asset gating filter updated for error page redirects
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@1935 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- bundle/src/main/java/com/hdscorp/cms/filters/AssetGatingFilter.java
+++ bundle/src/main/java/com/hdscorp/cms/filters/AssetGatingFilter.java
@@ -9,13 +9,16 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingFilter;
 import org.apache.felix.scr.annotations.sling.SlingFilterScope;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.adobe.acs.commons.errorpagehandler.ErrorPageHandlerService;
 import com.day.cq.wcm.api.WCMMode;
 import com.hdscorp.cms.config.HdsCorpGlobalConfiguration;
 import com.hdscorp.cms.util.HdsCorpCommonUtils;
@@ -32,6 +35,9 @@ import com.hdscorp.cms.util.HdsCorpCommonUtils;
 public class AssetGatingFilter implements Filter {
     private static final Logger log = LoggerFactory.getLogger(AssetGatingFilter.class);
 
+    @Reference
+    ErrorPageHandlerService errorPageHandlerService ;
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // Usually, do nothing
@@ -41,17 +47,17 @@ public class AssetGatingFilter implements Filter {
     public final void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
             ServletException {
     	
-    	final WCMMode mode = WCMMode.fromRequest(request);
+		if (!(request instanceof SlingHttpServletRequest) || !(response instanceof SlingHttpServletResponse)) {
+			// Not a SlingHttpServletRequest/Response, so ignore.
+			chain.doFilter(request, response);
+			return;
+		}
+    	
+		final SlingHttpServletResponse slingResponse = (SlingHttpServletResponse) response;
+		final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+
     	try {
 			
-			if (!(request instanceof SlingHttpServletRequest) || !(response instanceof SlingHttpServletResponse)) {
-				// Not a SlingHttpServletRequest/Response, so ignore.
-				chain.doFilter(request, response);
-				return;
-			}
-			
-			final SlingHttpServletResponse slingResponse = (SlingHttpServletResponse) response;
-			final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
 			String pdfPath= slingRequest.getRequestURI();
 			if(!pdfPath.contains("/editor") && !pdfPath.contains("/cf")&& pdfPath.toLowerCase().endsWith(".pdf") && (pdfPath.startsWith("/en-us/pdf") || pdfPath.startsWith("/content/dam/public/en_us/pdfs"))){
 				String forwardPath = (String)HdsCorpGlobalConfiguration.getPropertyValue(HdsCorpGlobalConfiguration.ASSET_GATING_FORM_PATH);
@@ -96,6 +102,28 @@ public class AssetGatingFilter implements Filter {
 				return;				
 			}			
     		
+		} catch (ResourceNotFoundException ex) {
+			
+			 if (errorPageHandlerService != null && errorPageHandlerService.isEnabled()) {
+			        // Check for and handle 404 Requests properly according on Author/Publish
+			        if (errorPageHandlerService.doHandle404(slingRequest, slingResponse)) {
+			             String path = errorPageHandlerService.findErrorPage(slingRequest, slingRequest.getResource());
+			            if (path != null) {
+			            	if(path.contains("500")) {
+			            		path = path.replace("500", "404");
+			            	}else if(!path.contains("404")){
+			            		path = path.replace(".html", "/404.html");
+			            	}
+			            	log.debug("Request path was -  "+slingRequest.getRequestURI());
+			            	log.debug("error page path=======**"+path);
+			                slingResponse.setStatus(404);
+			                errorPageHandlerService.includeUsingGET(slingRequest, slingResponse, path);
+			                return;
+			            }
+			        }
+			    }
+			
+			log.error("Asset Gating Filter ResourceNotFoundException Error Block - " + ex.getMessage()+" for the following path --- "+((SlingHttpServletRequest)request).getRequestURI());
 		} catch (Exception ex) {
 			log.error("Asset Gating Filter Error Block - " + ex.getMessage()+" for the following path --- "+((SlingHttpServletRequest)request).getRequestURI());
 		}
