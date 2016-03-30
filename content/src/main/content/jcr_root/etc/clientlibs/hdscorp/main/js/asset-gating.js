c8623e307b8a128b7778b92dd1017d6d31aebcd5
shashi.samir@hds.com <shashi.samir@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-03-30 23:19:47 +0000

    Asset Gated Page - Set Title of PDF to page
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2009 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/asset-gating.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/asset-gating.js
@@ -4,6 +4,7 @@ var hds = window.hds || {};
 (function(window, document, $, hds) {
     hds.assetGating = {
         init: function() {
+			$('title').text('HDS: ' + $('h4').text());
          },
          downloadPdf:function(){
         	 window.open(window.location.href+"?q=1");
