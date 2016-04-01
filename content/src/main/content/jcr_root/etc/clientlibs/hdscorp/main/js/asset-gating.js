ced3903a83f58b50f7b4e988e3ce64c367a6b69c
sandeep.kaur@hds.com <sandeep.kaur@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-04-01 00:25:55 +0000

    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2065 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/asset-gating.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/asset-gating.js
@@ -1,10 +1,11 @@
 /*Global Namespace*/
 var hds = window.hds || {};
-
+var parenRef="";
 (function(window, document, $, hds) {
     hds.assetGating = {
         init: function() {
 			$('title').text('HDS: ' + $('h4').text());
+			parenRef=document.referrer;
          },
          downloadPdf:function(){
         	 window.open(window.location.href+"?q=1");
