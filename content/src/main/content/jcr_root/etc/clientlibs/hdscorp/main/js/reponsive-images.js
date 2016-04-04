510706d5b7b871a400a376bd98ec7f5ebe7febbd
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-04-04 13:45:11 +0000

    lazyloading implemented for banner images
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2121 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/reponsive-images.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/reponsive-images.js
@@ -1,28 +1,55 @@
 $(function() {
+    var imgTargetClass = "rsImg";
+    var imglazyLoadCompletionClass = "lazyLoadComplete";
+    var windowSize = $(window).width();
+
+    var imageAttrName = "data-image-desktop" ;
+    if (windowSize <= 500) {
+    	imageAttrName = "data-image-mobile" ;
+    }
+
     function checkWidth() {
     	var imgName = "";
-        var windowSize = $(window).width();
-
-        if (windowSize <= 500) {
-			$('.rsImg').each(function (index, value){
-				imgName = $(this).attr('data-image-mobile');
+        var lazyLoadImages = false ;
+        lazyLoadImages = true ;
+        
+        if(!lazyLoadImages){
+			$('.'+imgTargetClass).each(function (index, value){
+				imgName = $(this).attr(imageAttrName);
 				$(this).css("background-image", "url('"+imgName+"')");
 			});
-			
 			return false;
-        }
-        else if (windowSize > 500) {
-		   $('.rsImg').each(function (index, value){
-			   imgName = $(this).attr('data-image-desktop');
-			   $(this).css("background-image", "url('"+imgName+"')");
-		   });
-
-		   return false;
+        }else{
+			$('.'+imgTargetClass).each(function (index, value){
+				$(this).on('inview', function(event, isInView) {
+					if (isInView && $(this).hasClass(imgTargetClass)) {
+						imgName = $(this).attr(imageAttrName);
+						$(this).css("background-image", "url('"+imgName+"')");
+						$(this).addClass(imglazyLoadCompletionClass);
+						$(this).removeClass(imgTargetClass);
+					}
+				});
+			});
+			return false;
         }
     }
 
-    // Execute on load
-    checkWidth();
-    // Bind event listener
-    $(window).resize(checkWidth);
-});
+    // Execute on load & scroll
+    $(window).on('load scroll', function() {
+    	checkWidth();
+    });
+    
+//    $(window).bind('resize', function() {
+//    	windowSize = $(window).width();
+//    	imageAttrName = "data-image-desktop" ;
+//        if (windowSize <= 500) {
+//        	imageAttrName = "data-image-mobile" ;
+//        }
+//    	$('.'+imglazyLoadCompletionClass).each(function (index, value){
+//    		$(this).addClass(imgTargetClass);
+//			$(this).removeClass(imglazyLoadCompletionClass);
+//			checkWidth();
+//    	});
+//    });
+
+});	
