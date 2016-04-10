3c06cff7caabb2bdbdcb5017b0a74f226d6d1e2e
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-04-10 04:30:36 +0000

    brightalk fix
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2392 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/hds-webcast.js
+++ content/src/main/content/jcr_root/etc/clientlibs/hdscorp/main/js/hds-webcast.js
@@ -98,7 +98,7 @@ var hds = window.hds || {};
                 var modal = $('#modal').modal();
                 var commId= $(this).attr('comid');
                 //modal.find('.modal-body').html('<script type="text/javascript" src="https://www.brighttalk.com/clients/js/embed/embed.js"></script><object class="BrightTALKEmbed" width="100%" height="627"><param name="player" value="webcast_player"/>   <param name="domain" value="https://www.brighttalk.com"/>   <param name="channelid" value="12821"/>   <param name="communicationid" value="'+commId+'"/>    <param name="autoStart" value="false"/>    <param name="theme" value=""/></object>');
-                modal.find('.modal-body').html('<script type="text/javascript" src="https://www.brighttalk.com/clients/js/embed/embed.js"></script><object class="BrightTALKEmbed" width="100%" height="627"><param name="player" value="webcast_player"/>   <param name="domain" value="https://www.brighttalk.com"/>   <param name="channelid" value="12821"/>   <param name="communicationid" value="'+commId+'"/>    <param name="autoStart" value="false"/>    <param name="theme" value="core.3.1.0/generic.swf"/></object>');
+                modal.find('.modal-body').html('<script type="text/javascript" src="https://www.brighttalk.com/clients/js/embed/embed.js"></script><object class="BrightTALKEmbed" width="'+$(".modal-body").width()+'" height="627"><param name="player" value="webcast_player"/>   <param name="domain" value="https://www.brighttalk.com"/>   <param name="channelid" value="12821"/>   <param name="communicationid" value="'+commId+'"/>    <param name="autoStart" value="false"/>    <param name="theme" value="core.3.1.0/generic.swf"/></object>');
                 modal.show();
                 return false;
             })
