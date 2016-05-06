f8689b8860061aaf08ad75813b1598eb87f834ee
sandeep.kaur@hds.com <sandeep.kaur@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-05-06 19:19:19 +0000

    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2597 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- hitachi-iot/iot-content/src/main/content/jcr_root/etc/clientlibs/hitachiiot/analytics/js/iotanalytics.js
+++ hitachi-iot/iot-content/src/main/content/jcr_root/etc/clientlibs/hitachiiot/analytics/js/iotanalytics.js
@@ -6,10 +6,6 @@ data = data.replace(/\s{2,}/g,' ');
 data = data.toLowerCase();
 items = data.split(":");
 count = items.length;
-if(pageTitle!="Home")
-	{
-		data=data.substring(data.indexOf(":")+1);
-	}
 var activeLinkText="top";
 var isTabClicked=false;
 var desktopType="";
@@ -118,30 +114,12 @@ if(isErrorPage())
 		}
 	}
 	
-	if(isProductCategory())
-	{
-		setVirtualCategoryEvent();
-
-		var cLeftNav = $('ul[id=asideLinks]');
-		cLeftNav.each(function() {
-
-			var links = $(this).find("a");
-			links.each(function() {
-
-				$(this).click(function(){
-					setTimeout(function() {
-					setVirtualCategoryEvent();
-					 }, 1000);
-				 });
-			});
-		});
-	}
 	
 	var url=document.title; //$(location).attr('href');
 	if(!(url.indexOf("404")>-1 || url.indexOf("error")>-1))
 	{	
 		primaryCategory = $.trim(primaryCategory.toLowerCase());
-		if(primaryCategory != "Smart Cities" && primaryCategory != "Smart Industry" && primaryCategory != "home" && primaryCategory != "About Us" && primaryCategory != "Lumada by Hitachi" && primaryCategory != "Smart Energy"){primaryCategory="";}
+		if(primaryCategory != "smart cities" && primaryCategory != "smart industry" && primaryCategory != "home" && primaryCategory != "about us" && primaryCategory != "lumada by hitachi" && primaryCategory != "smart energy"){primaryCategory="";}
 		digitalData.page=
 			{
 				pageInfo:{
@@ -229,7 +207,32 @@ if(isErrorPage())
 		});                 
      }
     });	
-	
+	// Tab Scrolls	
+    $(window).on('scroll', function(){
+        var tabs = $(".stickNav-container");
+		var activeLink = tabs.find("a.active");
+        if(activeLink.text()!="" && activeLinkText!=activeLink.text())
+        {
+             activeLinkText=activeLink.text();
+             
+            	 setTimeout(function() {
+        		 
+                    if(activeLinkText==activeLink.text())
+                    {
+                    	if(!isTabClicked)
+                        {
+    					var tabTitle = "iot>tab-"+activeLink.text().toLowerCase().replace(/\s/g,"-")+"-scroll";
+                        tabClick(primaryCategory,tabTitle,pageTitle,"Tabscroll");
+                        }
+                        	else
+                                isTabClicked=false;
+                    }
+ 
+            	 }, delay);           
+        }
+
+       });
+	  
 	//header HDS logos
 	
 	$(".hitachi-logo").on("click", function() {
@@ -244,113 +247,45 @@ if(isErrorPage())
 		globalMenuClick("linkclick","return to top",pageTitle,"button","iot>return to top");
 	});		
 	
-
-	//hero banners for all pages  .btn-square-white
-	$(".common-hero-banner").each(function() {
+	
+	//Video and link buttons (Red) on middle banners on different pages
+	
+	$(".accordion-section-hero, .common-hero-banner, .market-leader-container").each(function() {
      
 		 var listitem = $(this).find("a");
 		 var url=$(location).attr('href');
-		 //var linktext = $(this).find('h1').text();
-		 var linkposition="";
-		 var eType="link";
-		 var eClass="";
-		 if( listitem.length>0)
-		 {
-			listitem.each(function() {
-				var linktext = "iot>hero-" + $(this).parents().find('h1').text();// extracting offer title
-				linkposition="iot>hero-" + getPageNameFromURL();
-				$(this).click(function(){
-					if(linktext=="iot>hero-" && !($(this).parent().parent().find("h2").text()=="")){linktext="iot>hero-" + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-					if(linktext=="iot>hero-" && !($(this).parent().parent().find("h3").text()=="")){linktext="iot>hero-" + $(this).parent().parent().find("h3").text() + "-" + $.trim($(this).text());}
-					if(url.indexOf("/contact.html")>-1){linktext="iot>hero-" + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-					if($(this).parent().parent().attr("class")=="page-not-found")
-					{var eClass=$(this).parent().parent().attr("class");linktext=$.trim($(this).text());linkposition="iot>panel-main-" + getPageNameFromURL();}
-					else {var eClass=$(this).parent().attr("class");}
-					if(eClass.indexOf("buy-through")!=-1){eType = "link";if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}
-					else if ( eClass.indexOf("btn-square-white")!= -1){eType = "button"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-					else if ( eClass.indexOf("second-link")!= -1){eType = "link"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-					else if ( eClass.indexOf("playVideoBox")!= -1){eType = "play icon"; if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}//linktext = $(this).parents().find('h1').text();}
-					else if ( eClass.indexOf("video-play-desktop")!= -1){eType = "play icon";if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}// linktext = $(this).parents().find('h1').text();}
-					if(linktext == "" || !linktext){linktext="iot>hero-" + $.trim($(this).text());}
-					linktext = $.trim(linktext);	
-					globalMenuClick("linkclick",linktext.toLowerCase(),pageTitle,eType,linkposition); });
-				$(this).mousedown(function(e){
-					if(e.which == 3){
-						if(linktext=="iot>hero-" && !($(this).parent().parent().find("h2").text()=="")){linktext="iot>hero-" + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-						if(linktext=="iot>hero-" && !($(this).parent().parent().find("h3").text()=="")){linktext="iot>hero-" + $(this).parent().parent().find("h3").text() + "-" + $.trim($(this).text());}
-						if(url.indexOf("/contact.html")>-1){linktext="iot>hero-" + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-						if($(this).parent().parent().attr("class")=="page-not-found")
-						{var eClass=$(this).parent().parent().attr("class");linktext=$.trim($(this).text());linkposition="iot>panel-main-" + getPageNameFromURL();}
-						else {var eClass=$(this).parent().attr("class");}
-						if(eClass.indexOf("buy-through")!=-1){eType = "link";if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}
-						else if ( eClass.indexOf("btn-square-white")!= -1){eType = "button"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-						else if ( eClass.indexOf("second-link")!= -1){eType = "link"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-						else if ( eClass.indexOf("playVideoBox")!= -1){eType = "play icon"; if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}//linktext = $(this).parents().find('h1').text();}
-						else if ( eClass.indexOf("video-play-desktop")!= -1){eType = "play icon";if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}// linktext = $(this).parents().find('h1').text();}
-						if(linktext == "" || !linktext){linktext="iot>hero-" + $.trim($(this).text());}
-						linktext = $.trim(linktext);		
-						globalMenuClick("linkclick",linktext.toLowerCase(),pageTitle,eType,linkposition);
-					} 			
-				});
-			});                 
-		 }
-	//}
-    });
-	
-	//Banner section image in middle of pages
-	
-	$(".accordion-section-hero").each(function() {
-     
-	 var url=document.title;
-	//if(url.indexOf("404")<0)
-	//{
-		 var listitem = $(this).find("a");
-		 var url=$(location).attr('href');
-		 var linktext = $(this).find('h1').text();
+		 var linktext = "";
 		 var linkposition="";
 		 var eType="link";
 		 var eClass="";
 		 var linkType="iot>panel-";
-		 var linkpanel = $(this).parents('.accordion-level').attr('id');
-		 if($(this).parents(".bannerCarsoul").attr("id") == "partnerCarsoul"){ linkType="iot>hero-";}
-		 if(linktext==""){linktext=$(this).find('h2').text();}
+		 var linkpanel = "";
 		 if( listitem.length>0)
 		 {
-			//console.log("list Items:" + listitem.length);
 			listitem.each(function() {
-				var linktext = linkType + $(this).parent().parent().parent().find('h2').text();// extracting offer title
-				linkposition=linkType + linkpanel + "-" + getPageNameFromURL();
 				$(this).click(function(){
-					if(linktext==""){linktext=linkType + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-					if(linktext==""){linktext=linkType + $(this).parent().parent().find("h3").text() + "-" + $.trim($(this).text());}
-					if(url.indexOf("/contact.html")>-1){linktext=linkType + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
+					linkpanel = $(this).parents('.accordion-level').attr('id');
+					if($(this).parents('.accordion-level').find('h1').text()!="")
+						{linktext = "iot>panel-" + $(this).parents('.accordion-level').find('h1').text();
+						linkposition="iot>panel-" + linkpanel + "-" + getPageNameFromURL();}
+					else if($(this).parents('.market-leader-container').attr('class').indexOf('market-leader-container')>-1)
+						{linktext = "iot>panel-" + $('.market-leader-container').find('h2').text();
+						linkposition="iot>panel-" + $('.market-leader-container').find('h2').text()+ "-" + getPageNameFromURL();}
+					else 
+						{linktext = "iot>hero-" + $(this).parents('.common-hero-banner').find('h1').text();
+						linkposition="iot>hero-" + getPageNameFromURL();}
+					
 					if($(this).parent().parent().attr("class")=="page-not-found")
 					{var eClass=$(this).parent().parent().attr("class");linktext=$.trim($(this).text());linkposition="iot>panel-main-" + getPageNameFromURL();}
-					else {var eClass=$(this).parent().attr("class");}
-					if(eClass.indexOf("buy-through")!=-1){eType = "link";if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}
-					else if ( eClass.indexOf("btn-square-white")!= -1){eType = "button"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-					else if ( eClass.indexOf("playVideoBox")!= -1){eType = "play icon"; if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}//linktext = $(this).parents().find('h1').text();}
-					else if ( eClass.indexOf("video-play-desktop")!= -1){eType = "play icon";if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}// linktext = $(this).parents().find('h1').text();}
-					if(linktext == "" || !linktext){linktext=linkType + $.trim($(this).text());}
+					else {var eClass=$(this).attr("class");}
+					if ( eClass.indexOf("btn-play-video")!= -1){eType = "play icon";if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}} 
+					else if ( eClass.indexOf("btn-square-red")!= -1)
+					{
+						eType = "button";if(!($(this).text()>-1)){linktext=linktext + "-" + $.trim($(this).text());linkposition="iot>panel-" + $('.market-leader-content').find('h2').text() + "-" + getPageNameFromURL();}
+					} 
+					if(linktext == "" || !linktext){linktext="iot>panel-" + $.trim($(this).text());}
 					linktext = $.trim(linktext);	
 					globalMenuClick("linkclick",linktext.toLowerCase(),pageTitle,eType,linkposition); });
-				$(this).mousedown(function(e){
-					if(e.which == 3){
-						if(linktext==""){linktext=linkType + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-						if(linktext==""){linktext=linkType + $(this).parent().parent().find("h3").text() + "-" + $.trim($(this).text());}
-						if(url.indexOf("/contact.html")>-1){linktext=linkType + $(this).parent().parent().find("h2").text() + "-" + $.trim($(this).text());}
-						if($(this).parent().parent().attr("class")=="page-not-found")
-						{var eClass=$(this).parent().parent().attr("class");linktext=$.trim($(this).text());linkposition="iot>panel-main-" + getPageNameFromURL();}
-						else {var eClass=$(this).parent().attr("class");}
-						if(eClass.indexOf("buy-through")!=-1){eType = "link";if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}
-						else if ( eClass.indexOf("btn-square-white")!= -1){eType = "button"; if(!(linktext.indexOf($.trim($(this).text()))>-1)){linktext=linktext + "-" + $.trim($(this).text());}}// linktext = $(this).parents().find('h1').text();}
-						else if ( eClass.indexOf("playVideoBox")!= -1){eType = "play icon"; if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}//linktext = $(this).parents().find('h1').text();}
-						else if ( eClass.indexOf("video-play-desktop")!= -1){eType = "play icon";if(!(linktext.indexOf("-play icon")>-1)){linktext=linktext + "-play icon";}}// linktext = $(this).parents().find('h1').text();}
-						if(linktext == "" || !linktext){linktext=linkType + $.trim($(this).text());}
-						linktext = $.trim(linktext);		
-						globalMenuClick("linkclick",linktext.toLowerCase(),pageTitle,eType,linkposition);
-					} 			
-				});
 			});                 
 		 }
 	//}
@@ -359,10 +294,11 @@ if(isErrorPage())
 	// About us page partners logo
 	$("div.partner-list").each(function() 
 	{
+		
 		var links = $(this).find("img");
 		var headingtext=$('.partner-program .heading').find("h2").text();
 	   links.each(function() {
-		 var linktext = $(this).attr("title");
+		 var linktext = $(this).attr("alt");
 		 linktext=$.trim(linktext);
          if(linktext!="" || linktext.length > 0)
 		 {
@@ -468,9 +404,7 @@ if(isErrorPage())
 	});		
 
 
-	//Tabs Custom Tracking tabbing-container :event (web cast on demand), storage, legal  page tabs
-	//$(".stickNav-container, .custom-nav-tabs .nav-tabs, .webcast-listing").each(function() {
-	// Detail page tabs (overview, resources, specifications etc) and event page on demand and events tabs
+	//Tabs Custom Tracking tabbing-container :smart city page
 	$(".stickNav-container").each(function() {
 	 	var links = $(this).find("a");
 	  links.each(function() {
@@ -482,34 +416,7 @@ if(isErrorPage())
             });
 	  	});
 	});
-
-	
-	
-    $(window).on('scroll', function(){
-        var tabs = $(".stickNav-container");
-		var activeLink = tabs.find("a.active");
-        if(activeLink.text()!="" && activeLinkText!=activeLink.text())
-        {
-             activeLinkText=activeLink.text();
-             
-            	 setTimeout(function() {
-        		 
-                    if(activeLinkText==activeLink.text())
-                    {
-                    	if(!isTabClicked)
-                        {
-    					var tabTitle = "iot>tab-"+activeLink.text().toLowerCase().replace(/\s/g,"-")+"-scroll";
-                        tabClick(primaryCategory,tabTitle,pageTitle,"Tabscroll");
-                        }
-                        	else
-                                isTabClicked=false;
-                    }
  
-            	 }, delay);           
-        }
-
-       });
-	   
 //globalMenuClick(eventname,triggername,page)
 	function globalMenuClick(eventName,triggerName,page,triggerType,Position)
 	{
@@ -550,15 +457,6 @@ if(isErrorPage())
 			  hierarchy=hierarchy+$.trim($(this).text());
 		});
 		
-		if(isProductDetail())
-		{
-			var vcategory=$.cookie("vcategory");
-			if(vcategory && vcategory!='undefined' && vcategory!='null')
-			{
-				hierarchy=hierarchy+":"+vcategory;
-			}
-			$.cookie("vcategory",null,{ path: '/' });
-		}
 		if(pageTitle!="Home")
 		{
 			if(hierarchy.length>0)
@@ -568,22 +466,6 @@ if(isErrorPage())
 		return hierarchy;
 	}
 
-	function isProductDetail()
-	{
-		if($("body.productdetail").size()>0)
-			return true;
-		else
-			return false;
-	}
-	
-	function isProductCategory()
-	{
-		if($("body.productcategory").size()>0)
-			return true;
-		else
-			return false;
-	}
-	
 	// Get page name from URL for primary part
 	function getPageNameFromURL()
 	{
@@ -593,24 +475,6 @@ if(isErrorPage())
 		n=fileName.lastIndexOf(".");
 		if(n>0){fileName=fileName.substring(0, n);}
 		return fileName;
-	}
-	
-	function setVirtualCategoryEvent()
-	{
-		var cListing = $('.category-products-listing');
-		cListing.each(function() {
-		 var listitem = $(this).find("a");
-		 if( listitem.length>0)
-		 {
-			 listitem.each(function() {
-				 var linktext = $(this).text();
-				 $(this).click(function(){
-					   var activeCategory=$('li[class="linkLeft active"').find("a");
-					  $.cookie("vcategory",$.trim(activeCategory.text()),{ path: '/' });
-				 });
-			 });                 
-		  }
-		});
 	}
 		
 	function videoTracking(vId, pPageName)
