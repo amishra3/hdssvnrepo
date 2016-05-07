e4e454ad7310b7d50e81ba0499662ac0ba9d0d69
abhinav.mishra@epsilon.com <abhinav.mishra@epsilon.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-05-07 03:52:45 +0000

    video handler js method updated for https env
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2601 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- hitachi-iot/iot-content/src/main/content/jcr_root/etc/clientlibs/hitachiiot/main/js/iot-homepage-market-leader.js
+++ hitachi-iot/iot-content/src/main/content/jcr_root/etc/clientlibs/hitachiiot/main/js/iot-homepage-market-leader.js
@@ -10,7 +10,6 @@ if ($marketLeader.length) {
 
 
 function openvideooverlayById(videoID) {
-	
     var videoID = videoID;
     var pPageName = window.location.href;
     var videoGUID = "video"+videoID;
@@ -18,7 +17,11 @@ function openvideooverlayById(videoID) {
     if($.trim(videoID).length > 0){
         //var vidObjMkup = $(".bcobjmarkup").html();
        // vidObjMkup = vidObjMkup.replace(/#videoTitleId/g,videoID).replace("#videoGuid",videoGUID);
-        vidObjMkup = '<object id="myExperience'+videoID+'" class="BrightcoveExperience"><param name="linkBaseURL" value="'+pageUrl+'#vid='+videoID+'"/><param name="bgcolor" value="#FFFFFF" />  <param name="width" value="920" />  <param name="height" value="517" />  <param name="playerID" value="'+videoID+'" /><param name="playerKey" value="AQ~~,AAADnJnNnnk~,ltuihYvDjRIOlM3eAv1n6tj_paXrEum1" />  <param name="isVid" value="true" />  <param name="isUI" value="true" />  <param name="dynamicStreaming" value="true" />    <param name="@videoPlayer" value="'+videoID+'" />  <param name="secureConnections" value="true" /><param name="secureHTMLConnections" value="true" /><param name="includeAPI" value="true" /><param name="templateLoadHandler" value="myTemplateLoaded" /></object>';
+    	var secureBCParams = "" ;
+    	if (window.location.protocol === 'https:') {
+    		secureBCParams = '<param name="secureConnections" value="true" /><param name="secureHTMLConnections" value="true" />';
+    	}
+        vidObjMkup = '<object id="myExperience'+videoID+'" class="BrightcoveExperience"><param name="linkBaseURL" value="'+pageUrl+'#vid='+videoID+'"/><param name="bgcolor" value="#FFFFFF" />  <param name="width" value="920" />  <param name="height" value="517" />  <param name="playerID" value="'+videoID+'" /><param name="playerKey" value="AQ~~,AAADnJnNnnk~,ltuihYvDjRIOlM3eAv1n6tj_paXrEum1" />  <param name="isVid" value="true" />  <param name="isUI" value="true" />  <param name="dynamicStreaming" value="true" />    <param name="@videoPlayer" value="'+videoID+'" />  <param name="secureConnections" value="true" /><param name="secureHTMLConnections" value="true" /><param name="includeAPI" value="true" /><param name="templateLoadHandler" value="myTemplateLoaded" />'+secureBCParams+'</object>';
         videobox = new HDS.Lightbox();
         gblPlayingVideo = undefined ; 
         videobox.setContent('');
