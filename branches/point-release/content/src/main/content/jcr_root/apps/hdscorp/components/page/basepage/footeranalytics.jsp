1b7a09c9734b9772fc93f7d0c16cf750f5b28aa9
kumar.subramaniam@hds.com <kumar.subramaniam@hds.com@b06f84e9-1d16-493f-8c0d-41b76f67d7c8>
2016-05-16 23:28:32 +0000

    deleting branch inside hdscorp
    git-svn-id: http://vreportetl01t.corp.hds.com/svnrepo/HDS.com-Revamp/hdscorp@2630 b06f84e9-1d16-493f-8c0d-41b76f67d7c8

--- branches/point-release/content/src/main/content/jcr_root/apps/hdscorp/components/page/basepage/footeranalytics.jsp
+++
@@ -1,5 +0,0 @@
-<%@include file="/apps/foundation/global.jsp"%>
-    <script type="text/javascript">
-    if(typeof _satellite != 'undefined')
-    	_satellite.pageBottom();
-    </script>
--- branches/point-release/hitachi-iot/iot-content/src/main/content/jcr_root/etc/clientlibs/hitachiiot/main/js/iot-homepage-hero.js
+++
@@ -1,6 +0,0 @@
-// Set hero and children element heights to match
-//
-$(window).on("resize", function () {
-  var highestCol = Math.max($('.iot-hero').height(),$('.hero-banner').height());
-  $('.hero-banner, .hero-banner>img').height(highestCol);
-}).resize();
